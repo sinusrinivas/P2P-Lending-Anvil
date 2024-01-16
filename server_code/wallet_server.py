ebfdc2905dea370a0dbc9933ad19e97fea766120
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2024-01-16 12:20:56 +0000

    Edited settings and wallet_server

--- server_code/wallet_server.py
+++ server_code/wallet_server.py
@@ -6,7 +6,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
-from datetime import datetime
+from datetime import datetime  
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
