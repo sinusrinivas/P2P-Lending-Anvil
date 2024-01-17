64cab956beefaa6c23fdb06d50abcaee2dd971b7
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-01-17 08:26:31 +0000

    Created lendor_registration_form.dashboard.view_lost_oppurtunities, lendor_registration_form.dashboard.view_lost_oppurtunities.RowTemplate34 and lendor_registration_form.dashboard.view_lost_oppurtunities.view_details_2; Deleted lendor_registration_form.dashboard.vlo.RowTemplate34, lendor_registration_form.dashboard.vlo and lendor_registration_form.dashboard.vlo.view_details_2

--- client_code/lendor_registration_form/dashboard/view_lost_oppurtunities/__init__.py
+++ client_code/lendor_registration_form/dashboard/view_lost_oppurtunities/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import vloTemplate
+from ._anvil_designer import view_lost_oppurtunitiesTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -10,12 +10,12 @@ from anvil.tables import app_tables
 from .. import lendor_main_form_module as main_form_module
 
 
-class vlo(vloTemplate):
+class view_lost_oppurtunities(view_lost_oppurtunitiesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.user_id = main_form_module.userId
-    self.repeating_panel_2.items = app_tables.fin_loan_details.search(loan_updated_status=q.like('lost oppurtunities%'))
+    self.repeating_panel_2.items = app_tables.loan_details.search(loan_updated_status=q.like('lost oppurtunities%'))
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
