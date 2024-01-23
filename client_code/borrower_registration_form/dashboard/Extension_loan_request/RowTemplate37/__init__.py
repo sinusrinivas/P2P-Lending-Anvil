fc24e3b59103b7a762d156481bcbecacb5f80c53
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-01-23 12:24:26 +0000

    Created borrower_registration_form.dashboard.Extension_loan_request, borrower_registration_form.dashboard.Extension_loan_request.borrower_extension.extension2, borrower_registration_form.dashboard.Extension_loan_request.RowTemplate37 and borrower_registration_form.dashboard.Extension_loan_request.borrower_extension; Edited settings and borrower_registration_form.dashboard

---
+++ client_code/borrower_registration_form/dashboard/Extension_loan_request/RowTemplate37/__init__.py
@@ -0,0 +1,20 @@
+from ._anvil_designer import RowTemplate37Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class RowTemplate37(RowTemplate37Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    selected_row = self.item
+    open_form('borrower_registration')
