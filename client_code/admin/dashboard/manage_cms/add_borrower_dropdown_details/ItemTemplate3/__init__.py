c07e2be53c1a57bab5fa4856c703a7f6d79bcd80
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-01-24 11:13:41 +0000

    Edited admin.dashboard.manage_cms.add_borrower_dropdown_details.ItemTemplate3

--- client_code/admin/dashboard/manage_cms/add_borrower_dropdown_details/ItemTemplate3/__init__.py
+++ client_code/admin/dashboard/manage_cms/add_borrower_dropdown_details/ItemTemplate3/__init__.py
@@ -12,9 +12,22 @@ class ItemTemplate3(ItemTemplate3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.data_row = None  # Keep track of the data row associated with this item
+    self.edit_mode = False
     # Any code you write here will run before the form opens.
 
   def gender_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if self.edit_mode:
+            # If already in edit mode, save the changes
+            edited_data = self.text_box_data.text
+            self.data_row['data_column'] = edited_data
+            self.edit_mode = False
+            Notification("Data saved successfully")
+    else:
+            # If not in edit mode, enter edit mode
+            self.edit_mode = True
+
+  def gender_button_copy_click(self, **event_args):
     """This method is called when the button is clicked"""
     
