5dd19eea6011cb33795e9ba5b305202dd04df797
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-01-30 07:10:42 +0000

    Created admin.dashboard.home_button_admin; Edited admin.dashboard.admin_teams, admin.dashboard.admin_teams.view_profile, admin.dashboard.admin_teams.view_profile.edit_form, admin.dashboard.admin_teams.view_profile.update_form and 70 more

---
+++ client_code/admin/dashboard/home_button_admin/__init__.py
@@ -0,0 +1,19 @@
+from ._anvil_designer import home_button_adminTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class home_button_admin(home_button_adminTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_1_click(self, **event_args):
+    open_form('admin.dashboard')
