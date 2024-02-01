7389626f54dc00dc1499ef12e9c3b58f18567ae2
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2024-02-01 16:28:30 +0000

    Created borrower_registration_form.star_1_borrower_registration_form_3_marital.star_1_borrower_registration_form_3_marital_married; Deleted borrower_registration_form.star_1_borrower_registration_form_3_marital.star_1_borrower_registration_form_3_marital_spouse_3, borrower_registration_form.star_1_borrower_registration_form_3_marital.star_1_borrower_registration_form_3_marital_spouse_2, borrower_registration_form.star_1_borrower_registration_form_3_marital.star_1_borrower_registration_form_3_marital_spouse_1 and borrower_registration_form.star_1_borrower_registration_form_3_marital.star_1_borrower_registration_form_3_marital_parent

---
+++ client_code/borrower_registration_form/star_1_borrower_registration_form_3_marital/star_1_borrower_registration_form_3_marital_married/__init__.py
@@ -0,0 +1,58 @@
+from ._anvil_designer import star_1_borrower_registration_form_3_marital_marriedTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class star_1_borrower_registration_form_3_marital_married(star_1_borrower_registration_form_3_marital_marriedTemplate):
+  def __init__(self,user_id, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.userId = user_id
+    self.another_person = None
+
+    # Any code you write here will run before the form opens.
+
+  def radio_button_change(self, **event_args):
+    selected_button = self.radio_buttons.selected_button
+    self.another_person = selected_button.text.lower()
+
+    # Hide other radio buttons
+    for button in self.radio_buttons.components:
+      if button != selected_button:
+        button.visible = False
+
+    # Call the server function to update the database
+        anvil.server.call('update_another_person', self.userId, self.another_person)
+
+  def button_submit_click(self, **event_args):
+    # Check the selected radio button and show the corresponding grid panel
+    if self.another_person == 'Father':
+      self.grid_panel_1.visible = True
+      self.grid_panel_2.visible = False
+      self.grid_panel_3.visible = False
+      self.grid_panel_4.visible = False
+    elif self.another_person == 'Mother':
+      self.grid_panel_1.visible = False
+      self.grid_panel_2.visible = True
+      self.grid_panel_3.visible = False
+      self.grid_panel_4.visible = False
+    elif self.another_person == 'Spouse':
+      self.grid_panel_1.visible = False
+      self.grid_panel_2.visible = False
+      self.grid_panel_3.visible = True
+      self.grid_panel_4.visible = False
+    elif self.another_person == 'Other':
+      self.grid_panel_1.visible = False
+      self.grid_panel_2.visible = False
+      self.grid_panel_3.visible = False
+      self.grid_panel_4.visible = True
+
+  def button_1_click(self, **event_args):
+    open_form('borrower_registration_form.star_1_borrower_registration_form_3_marital.star_1_borrower_registration_form_3_marital_married',user_id=self.userId)
+
+    # Any code you write here will run before the form opens.
