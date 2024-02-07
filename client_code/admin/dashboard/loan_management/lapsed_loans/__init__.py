1f1da047e58d91223e3634fc7a98b54b48a2d995
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-02-07 07:55:38 +0000

    Edited settings, admin.dashboard.loan_management.not_payable_amount, borrower_registration_form.dashboard.today_dues and admin.dashboard.loan_management.lapsed_loans

--- client_code/admin/dashboard/loan_management/lapsed_loans/__init__.py
+++ client_code/admin/dashboard/loan_management/lapsed_loans/__init__.py
@@ -48,43 +48,6 @@ class lapsed_loans(lapsed_loansTemplate):
 
       self.repeating_panel_1.items = self.result
 
-  # def __init__(self, **properties):
-  #   # Set Form properties and Data Bindings.
-  #   self.init_components(**properties)
-
-  #   self.data = tables.app_tables.fin_loan_details.search()
-
-  #   a = -1
-  #   self.list_1 = []
-  #   self.list_2 = []
-  #   self.list_3 = []
-  #   self.list_4 = []
-    
-    
-  #   for i in self.data:
-  #     a+=1
-  #     self.list_1.append(i['loan_id'])
-  #     self.list_2.append(i['borrower_customer_id'])
-  #     self.list_3.append(i['borrower_full_name'])
-  #     self.list_4.append(i['loan_updated_status'])
-
-
-  #   self.result = []
-  #   self.index = []
-  #   if a == -1:
-  #     alert("No Data Available Here!")
-  #   else:
-  #     b = -1
-  #     for i in self.list_4:
-  #       b+=1
-  #       if i == "lapsed loan" or i == 'Lapsed loan' or i == "Lapsed Loan" or i == 'lapsed' or i == 'Lapsed':
-  #         self.index.append(b)
-          
-  #     for i in self.index:
-  #       self.result.append({'loan_id' : self.list_1[i], 'coustmer_id' : self.list_2[i], 'full_name' : self.list_3[i], 'loan_status' : self.list_4[i]})
-
-  #     self.repeating_panel_1.items = self.result
-
 
   def button_1_copy_click(self, **event_args):
     """This method is called when the button is clicked"""
