5c43fb79026c1db58f5513cf68889cfb48ba8915
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-02-08 08:08:55 +0000

    Created admin.dashboard.loan_management.one_time_settlement.view_profile_10, admin.dashboard.loan_management.one_time_settlement.ItemTemplate26 and admin.dashboard.loan_management.one_time_settlement; Edited admin.dashboard.loan_management

---
+++ client_code/admin/dashboard/loan_management/one_time_settlement/ItemTemplate26/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import ItemTemplate26Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class ItemTemplate26(ItemTemplate26Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    value_to_pass = self.label_1.text
+    open_form('admin.dashboard.loan_management.one_time_settlement.view_profile_10', value_to_pass)
