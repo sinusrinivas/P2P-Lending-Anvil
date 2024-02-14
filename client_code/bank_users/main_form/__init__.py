1781b968e91c3dcea4c94a1b09cfa30deda271bb
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2024-02-14 07:54:04 +0000

    Edited admin.dashboard and bank_users.main_form

--- client_code/bank_users/main_form/__init__.py
+++ client_code/bank_users/main_form/__init__.py
@@ -20,49 +20,44 @@ class main_form(main_formTemplate):
     # Any code you write here will run before the form opens.
   
   def login_signup_button_click(self, **event_args):
-    anvil.users.login_with_form()
-    current_user = anvil.users.get_user()
-    if current_user:
-      user_email = current_user['email']
-      print(user_email)
-      check_user_already_exist = user_module.check_user_profile(user_email)
-      print(check_user_already_exist)
-      if check_user_already_exist == None:
-        print("main if statement was executed")
-        user_module.add_email_and_user_id(user_email)
-        main_form_module.email = user_email
-        main_form_module.flag = True
-        open_form('bank_users.basic_registration_form')
-      else:
-        check_user_registration = user_module.check_user_registration_form_done_or_not_engine(user_email)
-        print("main else statement was executed")
-        if check_user_registration:
-          user_profile_e = app_tables.fin_user_profile.get(email_user=user_email)
-          main_form_module.email = user_email
-          main_form_module.userId = user_module.find_user_id(user_email)
-          if user_profile_e is not  None:
-            user_type = user_profile_e['usertype']
-            if user_type == 'lender':
-              open_form('lendor_registration_form.dashboard')
-            elif user_type == 'borrower':
-              if user_profile_e['one_time_settlement'] != True:
-                open_form('borrower_registration_form.dashboard')
-              elif user_profile_e['one_time_settlement'] == True:
-                open_form('borrower_registration_form.ots_dashboard')
-              # else:
-              #   open_form('bank_users.user_form')
-            elif user_type == 'admin':
-              open_form('admin.dashboard')
+        anvil.users.login_with_form()
+        current_user = anvil.users.get_user()
+        if current_user:
+            user_email = current_user['email']
+            print(user_email)
+            check_user_already_exist = user_module.check_user_profile(user_email)
+            print(check_user_already_exist)
+            if check_user_already_exist is None:
+                print("main if statement was executed")
+                user_module.add_email_and_user_id(user_email)
+                main_form_module.email = user_email
+                main_form_module.flag = True
+                open_form('bank_users.basic_registration_form')
             else:
-              open_form('bank_users.user_form')
+                check_user_registration = user_module.check_user_registration_form_done_or_not_engine(user_email)
+                print("main else statement was executed")
+                user_profile_e = app_tables.fin_user_profile.get(email_user=user_email)
+                main_form_module.email = user_email
+                main_form_module.userId = user_module.find_user_id(user_email)
+                if user_profile_e is not None:
+                    user_type = user_profile_e['usertype']
+                    if user_type == 'admin':
+                        open_form('admin.dashboard')
+                    elif user_type == 'lender':
+                        open_form('lendor_registration_form.dashboard')
+                    elif user_type == 'borrower':
+                        if user_profile_e['one_time_settlement'] != True:
+                            open_form('borrower_registration_form.dashboard')
+                        elif user_profile_e['one_time_settlement'] == True:
+                            open_form('borrower_registration_form.ots_dashboard')
+                    else:
+                        open_form('bank_users.basic_registration_form')
+                else:
+                    # Handle the case when user_profile_e is None
+                    open_form('bank_users.user_form')
         else:
-          main_form_module.email = user_email
-          main_form_module.flag = False
-          user_type = None
-          if user_type == '' :
-            open_form('bank_users.basic_registration_form')
-          else:
-            open_form('admin.dashboard')
+            # Handle the case when current_user is None
+            open_form('bank_users.user_form')
 
 #-- imp logic dont go up--#
 
