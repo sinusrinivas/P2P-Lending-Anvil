fde58b8c727ad6f667052b6adf3cd593d1262e5a
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2024-02-14 05:37:44 +0000

    Edited bank_users.main_form

--- client_code/bank_users/main_form/__init__.py
+++ client_code/bank_users/main_form/__init__.py
@@ -58,6 +58,7 @@ class main_form(main_formTemplate):
         else:
           main_form_module.email = user_email
           main_form_module.flag = False
+          user_type = None
           if user_type == 'lender' or user_type=='borrower':
             open_form('bank_users.basic_registration_form')
           else:
