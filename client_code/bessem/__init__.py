3979799af917f0738e9505e999a3fd60d70a9a47
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2024-02-19 04:50:46 +0000

    Edited borrower_server and bessem

--- client_code/bessem/__init__.py
+++ client_code/bessem/__init__.py
@@ -9,37 +9,38 @@ from anvil.tables import app_tables
 
 # user data 
 
-#users = app_tables.fin_user_profile.search(email_user=)
-
 # this function is force push to total points in bessem table
-def final_points_update_bessem_table():
-  final_points = general_points()+loan_points()+loan_and_trust_user_model()+occupation_point_and_government_id_model()
-  general_check = fake_detection()
+def final_points_update_bessem_table(user_id):
+  id = user_id
+  final_points = general_points(id)+loan_points(id)+loan_and_trust_user_model(id)+occupation_point_and_government_id_model(id)
+  general_check = fake_detection(id)
   if general_check:
     if final_points<= 1000:
       return final_points
     else:
-      fake_control()
+      fake_control(id)
   else:
-    block_user()
+    block_user(id)
 
- 
+
+
 # caluculate from user_profile table
-def general_points():
+def general_points(id):
+  users = app_tables.fin_user_profile.search(customer_id=i)
   return 400
 
 
 
 # this function is working on underwriting 
-def loan_points():
+def loan_points(id):
   return 100
 
 # this function is working on underwriting
-def loan_and_trust_user_model():
+def loan_and_trust_user_model(id):
   return 200
 
 # this function have to check by admin
-def occupation_point_and_government_id_model():
+def occupation_point_and_government_id_model(id):
   
   return 300
 
@@ -51,16 +52,16 @@ def occupation_point_and_government_id_model():
 # this function goes to be create
 
 
-def fake_control():
+def fake_control(id):
   return 500
 
 
-def fake_detection():
+def fake_detection(id):
   
   return True
 
 
-def block_user():
+def block_user(id):
   pass
   # if this function is call the is going to be block
 
