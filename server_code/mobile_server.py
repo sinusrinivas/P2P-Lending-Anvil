cad483a46b303a1222c86982d03bf18c72a442b4
bejawadaprem@gmail.com <bejawadaprem@gmail.com>
2024-02-22 11:47:39 +0000

    Edited mobile_server

--- server_code/mobile_server.py
+++ server_code/mobile_server.py
@@ -20,8 +20,8 @@ def get_table_data():
     return data
 
 @anvil.server.callable
-def add_data(customer_id, email, password, name, number):
-  tables.app_tables.users.add_row(email=email, password_hash=password)
+def add_data(customer_id, email, password, name, number, enable):
+  tables.app_tables.users.add_row(email=email, password_hash=password, enabled=enable)
   tables.app_tables.fin_user_profile.add_row(customer_id=customer_id, email_user=email, full_name=name, mobile=number)
 
 @anvil.server.callable
