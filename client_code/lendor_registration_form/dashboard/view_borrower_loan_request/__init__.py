4f8a28a14031167514ffbd6ba6bd1383d2c15330
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-03-04 16:38:20 +0000

    Created lendor_registration_form.dashboard.view_borrower_loan_request.ItemTemplate48; Edited lendor_registration_form.dashboard.view_borrower_loan_request and borrower_registration_form.dashboard.foreclosure_request.ItemTemplate46

--- client_code/lendor_registration_form/dashboard/view_borrower_loan_request/__init__.py
+++ client_code/lendor_registration_form/dashboard/view_borrower_loan_request/__init__.py
@@ -20,7 +20,7 @@ class view_borrower_loan_request(view_borrower_loan_requestTemplate):
     email = self.email
     
     
-    self.repeating_panel_1.items=app_tables.fin_loan_details.search(loan_updated_status="under process")
+    self.repeating_panel_2.items=app_tables.fin_loan_details.search(loan_updated_status="under process")
     
     anvil.server.call('transfer_user_profile_to_loan_details', email,self.user_id) 
 
