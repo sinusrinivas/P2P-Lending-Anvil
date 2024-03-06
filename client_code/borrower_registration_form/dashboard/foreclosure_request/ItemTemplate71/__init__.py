b96575a554454fd415290d7fcc1862f57a16a045
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-03-06 09:14:05 +0000

    Created borrower_registration_form.dashboard.foreclosure_request.ItemTemplate71; Edited bank_users.main_form, bank_users.repeating_panels.borrower, borrower_registration_form.dashboard.application_tracker.ItemTemplate69, borrower_registration_form.dashboard.foreclosure_request and 2 more

--- client_code/borrower_registration_form/dashboard/foreclosure_request/ItemTemplate71/__init__.py
+++ client_code/borrower_registration_form/dashboard/foreclosure_request/ItemTemplate71/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import ItemTemplate46Template
+from ._anvil_designer import ItemTemplate71Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -8,15 +8,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class ItemTemplate46(ItemTemplate46Template):
+class ItemTemplate71(ItemTemplate71Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    selected_row = self.item
-    open_form()
-
