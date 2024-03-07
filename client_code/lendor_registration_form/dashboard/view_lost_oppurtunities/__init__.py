8f1bb8bfe087b46db7abe655e8d04b659f82f53a
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-03-07 03:24:00 +0000

    Edited lendor_registration_form.dashboard.view_lost_oppurtunities and lendor_registration_form.dashboard.view_lost_oppurtunities.ItemTemplate53

--- client_code/lendor_registration_form/dashboard/view_lost_oppurtunities/__init__.py
+++ client_code/lendor_registration_form/dashboard/view_lost_oppurtunities/__init__.py
@@ -15,8 +15,19 @@ class view_lost_oppurtunities(view_lost_oppurtunitiesTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.user_id = main_form_module.userId
-    self.repeating_panel_1.items = app_tables.fin_loan_details.search(loan_updated_status=q.like('lost opportunities%'))
+    #self.repeating_panel_1.items = app_tables.fin_loan_details.search(loan_updated_status=q.like('lost opportunities%'))
+    lost_opportunities = app_tables.fin_loan_details.search(loan_updated_status=q.like('lost opportunities%'))
+    borrower_customer_ids = [loan['lender_customer_id'] for loan in lost_opportunities]
 
+        # Retrieve profiles for each borrower customer ID
+    borrower_profiles = []
+    for customer_id in borrower_customer_ids:
+            user_profile = app_tables.fin_user_profile.get(customer_id=customer_id)
+            if user_profile is not None:
+                borrower_profiles.append(user_profile.to_dict())
+
+        # Update UI with borrower profiles
+    self.repeating_panel_1.items = borrower_profiles
   
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
