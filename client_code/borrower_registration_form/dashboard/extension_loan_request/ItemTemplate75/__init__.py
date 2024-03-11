9f7f78eb0457c0ab541770f482abaef6677c97d3
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-03-11 08:31:43 +0000

    Edited borrower_registration_form.dashboard.extension_loan_request.ItemTemplate75, borrower_registration_form.dashboard.foreclosure_request.RowTemplate6, borrower_registration_form.dashboard.extension_loan_request.RowTemplate37 and borrower_registration_form.dashboard.foreclosure_request.ItemTemplate72

--- client_code/borrower_registration_form/dashboard/extension_loan_request/ItemTemplate75/__init__.py
+++ client_code/borrower_registration_form/dashboard/extension_loan_request/ItemTemplate75/__init__.py
@@ -1,29 +1,28 @@
 from ._anvil_designer import ItemTemplate75Template
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
+import anvil.google.auth
+import anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from .. import main_form_module as main_form_module
+from ... import main_form_module  # Adjust the import path accordingly
 
 
 class ItemTemplate75(ItemTemplate75Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    self.user_id=main_form_module.userId
-    user_data=app_tables.fin_user_profile.get(customer_id=self.user_id)
-    if user_data:
-      self.image_1.source= user_data['user_photo']
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        self.user_id = main_form_module.userId
+        user_data = app_tables.fin_user_profile.get(customer_id=self.user_id)
+        if user_data:
+            self.image_1.source = user_data['user_photo']
 
+        # Any code you write here will run before the form opens.
 
-    # Any code you write here will run before the form opens.
-
-  def outlined_button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    selected_row = self.item
-    open_form('borrower_registration_form.dashboard.extension_loan_request.borrower_extension', selected_row = selected_row)
-
+    def outlined_button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        selected_row = self.item
+        open_form('borrower_registration_form.dashboard.extension_loan_request.borrower_extension', selected_row=selected_row)
