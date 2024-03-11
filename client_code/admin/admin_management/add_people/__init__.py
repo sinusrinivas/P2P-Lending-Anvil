9be0e0cc0f650b5b03b378c0d7ba934b91d889e2
yarisingutarun@gmail.com <yarisingutarun@gmail.com>
2024-03-11 10:13:51 +0000

    Edited admin.admin_management.add_people

--- client_code/admin/admin_management/add_people/__init__.py
+++ client_code/admin/admin_management/add_people/__init__.py
@@ -28,4 +28,18 @@ class add_people(add_peopleTemplate):
     retype = self.re_enter_password.text
     created_date = date.today()
     if email and name and mobile_no and dob and gender and role and password:
-      result = admin.add_admin_details(email,name,mobile)
+      result = admin.add_admin_details(email,name,mobile_no,dob,gender,role,password,created_date)
+      if result:
+        self.label_6 = True
+        self.label_6.text = "Data added Successfully"
+      else:
+        self.label_6 = True
+        self.label_6.text = "User already exist"
+    else:
+      alert("fill all the fields")
+
+  def clear_function_click(self, **event_args):
+    open_form()
+      
+
+    
