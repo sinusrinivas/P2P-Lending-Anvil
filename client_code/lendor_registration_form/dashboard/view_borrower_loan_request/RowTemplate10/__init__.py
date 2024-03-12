ec8623ba05c4fa073e326fed31749efc9a714cd4
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-03-12 05:32:53 +0000

    Edited lendor_registration_form.dashboard.lender_view_loans.ItemTemplate27, lendor_registration_form.dashboard.lender_view_loans.ItemTemplate49, lendor_registration_form.dashboard.lender_view_loans.ItemTemplate50, lendor_registration_form.dashboard.lender_view_loans.ItemTemplate51 and 31 more

--- client_code/lendor_registration_form/dashboard/view_borrower_loan_request/RowTemplate10/__init__.py
+++ client_code/lendor_registration_form/dashboard/view_borrower_loan_request/RowTemplate10/__init__.py
@@ -5,13 +5,19 @@ import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 from anvil import open_form
+from .. import main_form_module as main_form_module
 
 
 class RowTemplate10(RowTemplate10Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        self.user_id=main_form_module.userId
+        user_data=app_tables.fin_user_profile.get(customer_id=self.user_id)
+        if user_data:
+          self.image_1.source= user_data['user_photo']
         
+          
 
     # Any code you write here will run before the form opens.
 
