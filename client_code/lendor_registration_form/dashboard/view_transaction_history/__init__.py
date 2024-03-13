31ca0b3bd53bf002d1a3e6ac0763fd605306bdad
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-03-13 16:26:39 +0000

    Edited borrower_registration_form.dashboard.view_transaction_history.ItemTemplate64, lendor_registration_form.dashboard.Module1, lendor_registration_form.dashboard.view_borrower_loan_request.Borr_loan_request, lendor_registration_form.dashboard.view_transaction_history and 3 more

--- client_code/lendor_registration_form/dashboard/view_transaction_history/__init__.py
+++ client_code/lendor_registration_form/dashboard/view_transaction_history/__init__.py
@@ -1,16 +1,66 @@
 from ._anvil_designer import view_transaction_historyTemplate
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .. import main_form_module as main_form_module
+
+# class view_transaction_history(view_transaction_historyTemplate):
+#     def __init__(self, **properties):
+#         # Set Form properties and Data Bindings.
+#         self.init_components(**properties)
+#         self.user_id=main_form_module.userId
+#         print(self.user_id)
+        
+#         # Fetch transaction_id associated with the provided customer_id
+#         transaction_ids = [row['transaction_id'] for row in app_tables.fin_wallet_transactions.search(customer_id=self.user_id)]
+        
+#         # Fetch transaction data based on transaction_ids
+#         self.result = []
+#         for transaction_id in transaction_ids:
+#             transaction_data = app_tables.fin_wallet_transactions.get(transaction_id=transaction_id)
+#             if transaction_data is not None:
+#                 borrower_profile = app_tables.fin_user_profile.get(email_user=transaction_data['user_email'])
+#                 lender_profile = app_tables.fin_user_profile.get(customer_id=transaction_data['receiver_customer_id'])
+#                 self.result.append({
+#                     'borrower_full_name': borrower_profile['full_name'],                    
+#                     'lender_full_name': lender_profile['full_name'],
+#                     'amount': transaction_data['amount'],
+#                     'transaction_time_stamp': transaction_data['transaction_time_stamp'],
+#                     'borrower_mobile': borrower_profile['mobile'], 
+#                     'lender_mobile': lender_profile['mobile']             
+#                 })
+
+#         self.repeating_panel_1.items = self.result
+
 
 class view_transaction_history(view_transaction_historyTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+        self.user_id = main_form_module.userId
+        print(self.user_id)
+        
+        # Fetch transaction_id associated with the provided customer_id
+        transaction_ids = [row['transaction_id'] for row in app_tables.fin_wallet_transactions.search(customer_id=self.user_id)]
+        
+        # Fetch transaction data based on transaction_ids
+        self.result = []
+        for transaction_id in transaction_ids:
+            transaction_data = app_tables.fin_wallet_transactions.get(transaction_id=transaction_id)
+            if transaction_data is not None:
+                borrower_profile = app_tables.fin_user_profile.get(email_user=transaction_data['user_email'])
+                lender_profile = app_tables.fin_user_profile.get(customer_id=transaction_data['receiver_customer_id'])
+                if borrower_profile is not None and lender_profile is not None:
+                    self.result.append({
+                        'borrower_full_name': borrower_profile['full_name'],                    
+                        'lender_full_name': lender_profile['full_name'],
+                        'amount': transaction_data['amount'],
+                        'transaction_time_stamp': transaction_data['transaction_time_stamp'],
+                        'borrower_mobile': borrower_profile['mobile'], 
+                        'lender_mobile': lender_profile['mobile']             
+                    })
 
-    # Any code you write here will run before the form opens.
+        self.repeating_panel_1.items = self.result
