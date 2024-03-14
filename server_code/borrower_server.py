f5b9934440941c612b12daf1e38f391c32883d04
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-03-14 03:54:01 +0000

    Edited borrower_server, settings and borrower_registration_form.dashboard.new_loan_request.loan_type

--- server_code/borrower_server.py
+++ server_code/borrower_server.py
@@ -124,7 +124,7 @@ def update_loan_details(loan_id, emi, total_repayment_amount, interest_rate):
         raise ValueError(f"Row not found for loan_id {loan_id}")
 
 @anvil.server.callable
-def add_loan_details(loan_amount, tenure,user_id,interest_rate, total_repayment_amount,product_id,membership_type,credit_limit,product_name,emi_payment_type,processing_fee_amount,total_interest,product_discription):
+def add_loan_details(loan_amount, tenure,user_id,interest_rate, total_repayment_amount,product_id,membership_type,credit_limit,product_name,emi_payment_type,processing_fee_amount,total_interest):
                                     
     
     # Generate a unique loan ID and get the updated counter
