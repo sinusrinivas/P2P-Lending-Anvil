e198ca0dd13b7d7e56efebe37d0193e03c84341f
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-03-15 05:36:00 +0000

    Edited borrower_registration_form.star_1_borrower_registration_form_2_employment.Form1

--- client_code/borrower_registration_form/star_1_borrower_registration_form_2_employment/Form1/__init__.py
+++ client_code/borrower_registration_form/star_1_borrower_registration_form_2_employment/Form1/__init__.py
@@ -15,3 +15,10 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    user_data = app_tables.fin_user_profile.get(customer_id=user_id)
+    if user_data:
+      self.drop_down_1.selected_value = user_data['self_employment']
+      user_data.update()
+    options = app_tables.fin_borrower_qualification.search()
+    options_string = [str(option['borrower_qualification']) for option in options]
+    self.drop_down_1.items = options_string
