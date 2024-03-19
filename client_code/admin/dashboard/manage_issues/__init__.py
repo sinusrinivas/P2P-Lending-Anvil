6a21a0826f75c516c51ed20ee5ab6d8074f3f6bc
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2024-03-19 15:36:48 +0000

    Edited admin.dashboard.manage_issues, admin.dashboard.manage_issues.ItemTemplate82, bank_users.basic_registration_form, borrower_registration_form.dashboard.dashboard_report_a_problem and 1 more

--- client_code/admin/dashboard/manage_issues/__init__.py
+++ client_code/admin/dashboard/manage_issues/__init__.py
@@ -8,9 +8,16 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
 class manage_issues(manage_issuesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    self.repeating_panel_1.items = [row for row in app_tables.fin_reported_problems.search() if not row['status']]
+    items = [row for row in app_tables.fin_reported_problems.search() if not row['status']]
+    self.repeating_panel_1.items = items
+        
+    if not items:
+        self.label_no_issues.visible = True
+    else:
+        self.label_no_issues.visible = False
