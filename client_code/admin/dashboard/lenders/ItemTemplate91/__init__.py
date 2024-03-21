ff705fc2e0302263fbbd79c4b5d709e9180569a7
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-03-21 04:22:57 +0000

    Edited admin.dashboard.borrowers.ItemTemplate83 and admin.dashboard.lenders.ItemTemplate91

--- client_code/admin/dashboard/lenders/ItemTemplate91/__init__.py
+++ client_code/admin/dashboard/lenders/ItemTemplate91/__init__.py
@@ -12,6 +12,15 @@ class ItemTemplate91(ItemTemplate91Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    user_data = app_tables.fin_loan_details.search()
+    for row in user_data:
+        borrower_customer_id = row['borrower_customer_id']
+        lender_customer_id = row['lender_customer_id']
+        borrower_profile = app_tables.fin_user_profile.get(customer_id=borrower_customer_id)
+        lender_profile = app_tables.fin_user_profile.get(customer_id=lender_customer_id)
+        self.image_1.source = lender_profile['user_photo']
+
+
 
     # Any code you write here will run before the form opens.
 
