571695a1174aee372a38c1a8ff7a7cfd4e059f26
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-04-04 05:35:14 +0000

    Edited admin.dashboard.manage_products.view_product.ItemTemplate98, settings, admin.dashboard.manage_products.view_product and admin.dashboard.manage_products.view_product.RowTemplate5

--- client_code/admin/dashboard/manage_products/view_product/ItemTemplate98/__init__.py
+++ client_code/admin/dashboard/manage_products/view_product/ItemTemplate98/__init__.py
@@ -14,3 +14,8 @@ class ItemTemplate98(ItemTemplate98Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    value_to_pass = self.link_1.text
+    open_form('admin.dashboard.manage_products.view_profile',value_to_pass)
