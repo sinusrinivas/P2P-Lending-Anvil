328140762daeb7a6e51d204e716bfb7b07c802d5
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-04-08 11:00:58 +0000

    Edited borrower_registration_form.dashboard.new_loan_request.check and borrower_registration_form.dashboard.new_loan_request.loan_type

--- client_code/borrower_registration_form/dashboard/new_loan_request/loan_type/__init__.py
+++ client_code/borrower_registration_form/dashboard/new_loan_request/loan_type/__init__.py
@@ -111,6 +111,7 @@ class loan_type(loan_typeTemplate):
         return app_tables.fin_product_details.search(
             product_group=self.proctct_g,
             product_categories=self.prodct_cate
+            product_name = 
         )
 
     def get_min_max_amount(self):
