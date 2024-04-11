fd580e3e1dacc2289b3da3bf723d4805ddedd328
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-04-11 07:31:21 +0000

    Edited lendor_registration_form.dashboard.today_dues

--- client_code/lendor_registration_form/dashboard/today_dues/__init__.py
+++ client_code/lendor_registration_form/dashboard/today_dues/__init__.py
@@ -16,7 +16,7 @@ class today_dues(today_duesTemplate):
         loan_details = []
 
         all_loans_disbursed = app_tables.fin_loan_details.search(
-            loan_updated_status=q.any_of("disbursed loan", "extension", "foreclose"),
+            loan_updated_status=q.any_of("disbursed loan", "extension", "foreclosure"),
             first_emi_payment_due_date=q.less_than_or_equal_to(today_date)
         )
         
@@ -204,3 +204,7 @@ class today_dues(today_duesTemplate):
     def home_borrower_registration_form_copy_1_click(self, **event_args):
         """This method is called when the button is clicked"""
         open_form('lendor_registration_form.dashboard')
+
+    def button_1_click(self, **event_args):
+      """This method is called when the button is clicked"""
+      open_form('lendor_registration_form.dashboard')
