24296381e260f7fd7475c12ad7465618fafc78f7
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2024-04-12 09:57:24 +0000

    Edited borrower_server

--- server_code/borrower_server.py
+++ server_code/borrower_server.py
@@ -85,8 +85,13 @@ def add_borrower_step6(bank_id, bank_branch, user_id):
         row[0]['bank_id'] = bank_id
         row[0]['account_bank_branch'] = bank_branch
         # bessem_value = bessemfunctions.final_points_update_bessem_table(user_id)
-        bessem_value = final_points_update_bessem_table(user_id)
-        row[0]['bessem_value'] = float(bessem_value)
+        
+        if bessem_value is not None:
+           row[0]['bessem_value'] = float(bessem_value)
+        else:
+           print("Warning: bessem_value is None for user_id:", user_id)
+        
+        # row[0]['bessem_value'] = float(bessem_value)
         row[0]['form_count'] = 6
         row[0]['usertype'] = 'borrower'
         row[0]['last_confirm'] = True
