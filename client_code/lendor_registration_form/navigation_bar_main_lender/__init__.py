8a28cddff20c83385280bf1ae8e23a76651d9d7e
anjalikalburgi9@gmail.com <anjalikalburgi9@gmail.com>
2024-04-17 11:42:47 +0000

    Edited borrower_registration_form.dashboard.Navigation_form_dashboard and lendor_registration_form.navigation_bar_main_lender

--- client_code/lendor_registration_form/navigation_bar_main_lender/__init__.py
+++ client_code/lendor_registration_form/navigation_bar_main_lender/__init__.py
@@ -7,17 +7,27 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...bank_users.main_form import main_form_module
 
 class navigation_bar_main_lender(navigation_bar_main_lenderTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.user_id=main_form_module.userId
+    user_id = self.user_id
 
     # Any code you write here will run before the form opens.
 
   def borrower_dashboard_home_linkhome_borrower_registration_button_copy_1_click(self, **event_args):
-    open_form("lendor_registration_form.dashboard")
+    # open_form("lendor_registration_form.dashboard")
+    user_request = app_tables.fin_user_profile.get(customer_id=self.user_id)
+    if user_request:
+      self.user_type = user_request['usertype']
 
+    if self.user_type == "lender":
+        open_form("lendor_registration_form.dashboard")
+    else:
+        open_form("bank_users.user_form")
 
   def report_problem_form_link_click(self, **event_args):
     """This method is called when the link is clicked"""
