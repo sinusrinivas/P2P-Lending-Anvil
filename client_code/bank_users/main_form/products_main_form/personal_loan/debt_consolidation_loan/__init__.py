7ea5da206d42e01aa04972aad0579496d1bba55e
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-18 10:55:19 +0000

    Edited settings, bank_users.main_form.products_main_form.personal_loan.debt_consolidation_loan and lendor_registration_form.dashboard.view_loan_extension_requests; Deleted lendor_registration_form.dashboard.view_loan_extension_requests.RowTemplate38

--- client_code/bank_users/main_form/products_main_form/personal_loan/debt_consolidation_loan/__init__.py
+++ client_code/bank_users/main_form/products_main_form/personal_loan/debt_consolidation_loan/__init__.py
@@ -14,3 +14,27 @@ class debt_consolidation_loan(debt_consolidation_loanTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def home_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def home_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bank_users.main_form')
+
+  def about_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bank_users.main_form.about_main_form')
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bank_users.main_form.products_main_form')
+
+  def contact_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bank_users.main_form.contact_main_form')
+
+  def link_6_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.user_issue.user_bugreports')
