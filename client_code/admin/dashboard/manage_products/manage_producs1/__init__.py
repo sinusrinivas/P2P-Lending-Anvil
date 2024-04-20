fbe132ad9a2f93bb3cd8596438247b5a145b3cbe
sandhyasiddu12@gmail.com <sandhyasiddu12@gmail.com>
2024-04-20 17:25:40 +0000

    Edited admin.dashboard.manage_products.manage_producs1

--- client_code/admin/dashboard/manage_products/manage_producs1/__init__.py
+++ client_code/admin/dashboard/manage_products/manage_producs1/__init__.py
@@ -309,9 +309,9 @@ class manage_producs1(manage_producs1Template):
             max_tenure = float(self.max_tenure.text.strip())
             roi = float(self.text_box_5.text.strip())
             lapsed_fee = float(self.lapsed_fee.text.strip())
-            default_fee = float(self.default_fee.text.strip() if self.default_fee.text.strip() else 0)
+            default_fee = float(self.default_fee.text.strip() if self.default_fee.text.strip() else None)
             default_fee_amount = float(self.text_box_1.text.strip() if self.text_box_1.text.strip() else 0)
-            npa = float(self.npa.text.strip() if self.npa.text.strip() else 0)
+            npa = float(self.npa.text.strip() if self.npa.text.strip() else None)
             npa_amount = float(self.text_amount.text.strip() if self.text_amount.text.strip() else 0)
 
     
