692355f64cfc755be2ef01274c477b06dbf4f258
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-23 12:03:37 +0000

    Edited bank_users.main_form.products_main_form.personal_loan, bank_users.main_form.products_main_form.personal_loan.credit_card_loan, bank_users.main_form.products_main_form.personal_loan.debt_consolidation_loan, bank_users.main_form.products_main_form.personal_loan.marriage_loan and 2 more; Deleted bank_users.main_form.product_main_form and bank_users.main_form.product_main_form.personal_loan

--- client_code/bank_users/main_form/products_main_form/personal_loan/__init__.py
+++ client_code/bank_users/main_form/products_main_form/personal_loan/__init__.py
@@ -8,6 +8,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
 class personal_loan(personal_loanTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
@@ -15,26 +16,18 @@ class personal_loan(personal_loanTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def home_main_form_link_click(self, **event_args):
+  def link_16_click(self, **event_args):
     """This method is called when the link is clicked"""
-    open_form('bank_users.main_form')
-
-  def about_main_form_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('bank_users.main_form.about_main_form')
-
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('bank_users.main_form.products_main_form')
-
-  def contact_main_form_link_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('bank_users.main_form.contact_main_form')
-
-  def link_6_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('admin.user_issue.user_bugreports')
+    open_form('bank_users.main_form.products_main_form.personal_loan.travel_loan')
 
   def link_4_click(self, **event_args):
     """This method is called when the link is clicked"""
     open_form('bank_users.main_form.products_main_form.personal_loan.marriage_loan')
+
+  def link_5_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bank_users.main_form.products_main_form.personal_loan.debt_consolidation_loan')
+
+  def link_14_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('bank_users.main_form.products_main_form.personal_loan.credit_card_loan')
