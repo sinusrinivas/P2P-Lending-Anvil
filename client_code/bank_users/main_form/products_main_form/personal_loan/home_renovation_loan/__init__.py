f14e0be931983c0fb45105b0f44565e9f43e1a93
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-24 06:09:57 +0000

    Created bank_users.main_form.products_main_form.personal_loan.home_renovation_loan and home loan 4.png

---
+++ client_code/bank_users/main_form/products_main_form/personal_loan/home_renovation_loan/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import home_renovation_loanTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class home_renovation_loan(home_renovation_loanTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
