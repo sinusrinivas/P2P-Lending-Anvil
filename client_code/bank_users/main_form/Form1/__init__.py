6c501d590e28076b738c043f8d9bed360f177bd3
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-24 10:31:37 +0000

    Edited settings, admin_server and bank_users.main_form.Form1

--- client_code/bank_users/main_form/Form1/__init__.py
+++ client_code/bank_users/main_form/Form1/__init__.py
@@ -8,24 +8,12 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from anvil.js import window
+from anvil import Link, ho
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    
-
-def button_mouseover(self, **event_args):
-    # Show dropdown content when hovering over the button
-    window.get_element_by_id('dropdown-content').style.display = 'block'
-
-def navigate_to_registration_form(self, **event_args):
-    # Call server function to navigate to registration form
-    anvil.server.call('navigate_to_registration_form')
-
-def navigate_to_loan_application_form(self, **event_args):
-    # Call server function to navigate to loan application form
-    anvil.server.call('navigate_to_loan_application_form')
-
+  
   
