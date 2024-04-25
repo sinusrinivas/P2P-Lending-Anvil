7568071d80d4183f60368184493f5b660f46d91e
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-25 04:00:48 +0000

    Edited settings, admin_server, bank_users.main_form.Form1 and new_file.js

--- theme/assets/new_file.js
+++ theme/assets/new_file.js
@@ -1,12 +1,10 @@
-// new_fil
-  
 function openInvestNowForm() {
-    anvil.server.call('open_invest_now_form')
-      .then(function() {
-          // Optional: Handle success, if needed
-      })
-      .catch(function(err) {
-          // Optional: Handle error, if needed
-          console.error("Error opening Invest Now form:", err);
-      });
+  anvil.server.call('open_invest_now_form')
+    .then(function() {
+        // Optional: Handle success, if needed
+    })
+    .catch(function(err) {
+        // Optional: Handle error, if needed
+        console.error("Error opening Invest Now form:", err);
+    });
 }
