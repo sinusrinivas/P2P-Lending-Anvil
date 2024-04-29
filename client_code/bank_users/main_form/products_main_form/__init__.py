7ac116ec66f14afbd8037fed64d5c9a3fb723c3b
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-29 09:57:50 +0000

    Edited bank_users.main_form.products_main_form.personal_loan, bank_users.main_form.investNow_applyForLoan, bank_users.main_form.products_main_form.business_loan and bank_users.main_form.products_main_form

--- client_code/bank_users/main_form/products_main_form/__init__.py
+++ client_code/bank_users/main_form/products_main_form/__init__.py
@@ -108,14 +108,14 @@ class products_main_form(products_main_formTemplate):
     """This method is called when the link is clicked"""
     open_form('bank_users.main_form.about_main_form')
 
-  def button_2_click(self, **event_args):
+  def button_10_copy_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('bank_users.main_form.products_main_form.business_loan')
+    open_form('bank_users.main_form.investNow_applyForLoan')
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('bank_users.main_form.products_main_form.personal_loan')
+    open_form('bank_users.main_form.investNow_applyForLoan')
 
-  def button_10_copy_click(self, **event_args):
+  def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('bank_users.main_form.investNow_applyForLoan')
