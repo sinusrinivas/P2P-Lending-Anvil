2388ac62d1a02fe7e5d9e745670fe7c1cab87048
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-04-30 12:52:14 +0000

    Edited borrower_registration_form.dashboard.foreclosure_request, bank_users.user_form.user_form_copy, borrower_registration_form.dashboard.foreclosure_request.borrower_foreclosure and bank_users.user_form

--- client_code/borrower_registration_form/dashboard/foreclosure_request/__init__.py
+++ client_code/borrower_registration_form/dashboard/foreclosure_request/__init__.py
@@ -1,68 +1,60 @@
 from ._anvil_designer import foreclosure_requestTemplate
 from anvil import *
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 from .. import main_form_module as main_form_module
 
-
 class foreclosure_request(foreclosure_requestTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
+        self.user_id = main_form_module.userId
+        print("user", self.user_id)
 
-        
-            # Fetch the userprofile record based on the current user's email
-            user_profile = app_tables.fin_user_profile.get(email_user=user['email'])
-            # Check if the user profile record is found
-            if user_profile:
-                # Access the user ID from the userprofile record
-                user_id = user_profile['customer_id']
-                print(user_id)
-                # Filter loan_details table based on the current user's ID and loan status
-                try:
-                    customer_loans = [loan for loan in app_tables.fin_loan_details.search(borrower_customer_id=user_id) if loan['loan_updated_status'] in ['disbursed loan', 'foreclosure']]
-                    loans = []
-                    for loan in customer_loans:
-                        if user_profile is not None:
-                            lender_photo = app_tables.fin_user_profile.get(customer_id=loan['lender_customer_id'])
-                            # Check if the loan product is eligible for foreclosure
-                            product_details_record = app_tables.fin_product_details.get(product_id=loan['product_id'])
-                            if product_details_record['foreclose_type'] == 'Eligible':
-                                loan_data = {
-                                    'mobile': lender_photo['mobile'],
-                                    'interest_rate': loan['interest_rate'],
-                                    'loan_amount': loan['loan_amount'],
-                                    'tenure': loan['tenure'],
-                                    'loan_disbursed_timestamp': loan['loan_disbursed_timestamp'],
-                                    'product_name': loan['product_name'],
-                                    'product_description': loan['product_description'],
-                                    'lender_full_name': loan['lender_full_name'],
-                                    'product_id': loan['product_id'],
-                                    'loan_id': loan['loan_id'],
-                                    'loan_updated_status': loan['loan_updated_status'],
-                                    'emi_payment_type': loan['emi_payment_type'],
-                                    'credit_limit' : loan['credit_limit'],
-                                    'foreclosure_type' : loan['foreclosure_type'],
-                                    'borrower_full_name' : loan['borrower_full_name'],
-                                    'user_photo':lender_photo['user_photo']
-                                }
-                                loans.append(loan_data)
+        # Fetch the user profile record based on the current user's ID
+        user_profile = app_tables.fin_user_profile.get(customer_id=self.user_id)
+        # Check if the user profile record is found
+        if user_profile:
+            # Filter loan_details table based on the current user's ID and loan status
+            try:
+                customer_loans = [loan for loan in app_tables.fin_loan_details.search(borrower_customer_id=self.user_id) if loan['loan_updated_status'] in ['disbursed loan', 'foreclosure']]
+                loans = []
+                for loan in customer_loans:
+                    if user_profile is not None:
+                        lender_photo = app_tables.fin_user_profile.get(customer_id=loan['lender_customer_id'])
+                        # Check if the loan product is eligible for foreclosure
+                        product_details_record = app_tables.fin_product_details.get(product_id=loan['product_id'])
+                        if product_details_record['foreclose_type'] == 'Eligible':
+                            loan_data = {
+                                'mobile': lender_photo['mobile'],
+                                'interest_rate': loan['interest_rate'],
+                                'loan_amount': loan['loan_amount'],
+                                'tenure': loan['tenure'],
+                                'loan_disbursed_timestamp': loan['loan_disbursed_timestamp'],
+                                'product_name': loan['product_name'],
+                                'product_description': loan['product_description'],
+                                'lender_full_name': loan['lender_full_name'],
+                                'product_id': loan['product_id'],
+                                'loan_id': loan['loan_id'],
+                                'loan_updated_status': loan['loan_updated_status'],
+                                'emi_payment_type': loan['emi_payment_type'],
+                                'credit_limit' : loan['credit_limit'],
+                                'foreclosure_type' : loan['foreclosure_type'],
+                                'borrower_full_name' : loan['borrower_full_name'],
+                                'user_photo':lender_photo['user_photo']
+                            }
+                            loans.append(loan_data)
 
-                    # Set the filtered data as the items for the repeating panel
-                    self.repeat.items = loans
-                except anvil.tables.TableError as e:
-                    print(f"Error: {e}")
-            else:
-                # Handle the case when no user profile record is found
-                print("User profile record not found for the current user.")
+                # Set the filtered data as the items for the repeating panel
+                self.repeat.items = loans
+            except anvil.tables.TableError as e:
+                print(f"Error: {e}")
         else:
-            # Handle the case when no user is logged in
-            print("No user logged in")
+            # Handle the case when no user profile record is found
+            print("User profile record not found for the current user.")
 
     def button_1_click(self, **event_args):
         """This method is called when the button is clicked"""
