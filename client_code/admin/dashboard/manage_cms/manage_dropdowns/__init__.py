5f03b8235e2dc4d52e1e71267840b4c97fa2c9f7
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-05-06 12:04:59 +0000

    Created drop-down-menu.png, dropdown-arrow.png, manage dropdown.png, manage mem.png and 1 more; Edited admin.dashboard.manage_settings, admin.dashboard.manage_products, admin.dashboard.manage_cms.manage_dropdowns and admin.dashboard.manage_cms

--- client_code/admin/dashboard/manage_cms/manage_dropdowns/__init__.py
+++ client_code/admin/dashboard/manage_cms/manage_dropdowns/__init__.py
@@ -14,21 +14,25 @@ class manage_dropdowns(manage_dropdownsTemplate):
     self.init_components(**properties)
  
 
-  def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    
-    open_form('admin.dashboard.manage_cms.add_borrower_dropdown_details')
 
-  def button_2_copy_click(self, **event_args):
-    """This method is called when the button is clicked"""
     
-    open_form('admin.dashboard.manage_cms.add_lender_dropdown_details')
 
   def button_1_copy_3_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('admin.dashboard.manage_cms')
 
 
-  def gender_btn_click(self, **event_args):
+  def button_9_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('admin.dashboard.manage_cms.add_borrower_dropdown_details')
+
+  def button_14_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('admin.dashboard.manage_cms.add_lender_dropdown_details')
+
+  def button_11_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('admin.dashboard.manage_cms.add_general_dropdowns')
+
+
+
