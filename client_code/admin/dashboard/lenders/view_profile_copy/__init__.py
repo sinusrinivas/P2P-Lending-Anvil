c5fc0bded4ea298d8dd6f916b88a13d2364b886b
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-05-06 08:22:44 +0000

    Edited admin.dashboard.lenders.view_profile_copy and admin.dashboard.borrowers.view_profile

--- client_code/admin/dashboard/lenders/view_profile_copy/__init__.py
+++ client_code/admin/dashboard/lenders/view_profile_copy/__init__.py
@@ -160,70 +160,142 @@ class view_profile_copy(view_profile_copyTemplate):
       if value_to_display in self.id_list:
         b = self.id_list.index(value_to_display)
         self.label_3.text = value_to_display
-        self.label_8.text = self.name_list[b]
-        self.label_9.text = bool(self.status_list[b])
-        self.label_39.text= self.gender_list[b]
-        self.label_40.text = (self.age_list[b])
-        self.label_41.text = self.dob_list[b]
-        self.label_44.text = self.mobile_list[b]
-        self.label_45.text = self.aadhar_list[b]
-        self.label_46.text = self.pan_list[b]
-        self.label_47.text = self.city_list[b]
-        self.label_48.text = self.email_user_list[b]
-        self.label_49.text = bool(self.last_confirm_list[b])
-        self.label_50.text = self.mobile_check_list[b]
-        self.label_51.text = self.mother_tongue_list[b]
-        self.label_52.text = self.mother_status_list[b]
-        # self.label_53.text = self.date_marrige_list[b]
-        self.label_54.text = self.space_name_list[b]
-        self.label_61.text = self.about_list[b]
-        self.label_63.text = bool(self.alets_list[b])
-        self.label_72.text = bool(self.terms_list[b])
-        self.label_69.text = self.qualification_list[b]
-        self.label_62.text = self.address_type_list[b]
-        self.label_71.text = self.street_list[b]
-        self.label_64.text = self.build_name_list[b]
-        self.label_66.text = self.house_no_list[b]
-        self.label_65.text = self.landmark_list[b]
-        self.label_68.text = self.pincode_list[b]
-        self.label_70.text = self.state_list[b]
-        self.label_55.text = self.spouse_number_list[b]
-        self.label_56.text = self.company_name_list[b]
-        self.label_57.text = self.company_adress_list[b]
-        self.label_58.text =  self.proffic_list[b]
-        self.label_59.text = self.user_type_list[b]
-        self.label_60.text = bool(self.approve_list [b])
-        self.label_74.text = self.another_email[b]
-        self.label_76.text = self.company_name[b]
-        self.label_77.text = self.organization_type[b]
-        self.label_79.text = self.employment_type[b]
-        self.label_81.text = self.business_no[b]
-        self.label_83.text = self.company_landmark[b]
-        self.label_85.text = self.company_address[b]
-        self.label_87.text = self.annual_salary[b]
-        self.label_89.text = self.designation[b]
-        self.label_91.text = self.account_name[b]
-        self.label_93.text = self.account_type[b]
-        self.label_95.text = self.account_number[b]
-        self.label_97.text = self.account_bank_branch[b]
-        # self.label_99.text = self.ifsc_code[b]
-        self.label_101.text = self.salary_type[b]
-        #self.label_103.text = self.select_bank[b]
-        # self.label_105.text = self.net_bank[b]
-        self.label_107.text = self.father_name[b]
-        self.label_109.text = self.father_age[b]
-        self.label_111.text = self.mother_name[b]
-        self.label_113.text = self.mother_age[b]
-        self.label_115.text = self.college_name[b]
-        self.label_117.text = self.college_id[b]
-        self.label_119.text = self.college_address[b]
-        # self.label_121.text = self.running_loan[b]
+
+        self.set_label_visibility(self.label_8,self.label_2, self.name_list[b])
+        self.set_label_visibility(self.label_9,self.label_4, bool(self.status_list[b]))
+        self.set_label_visibility(self.label_39,self.label_5, self.gender_list[b])
+        self.set_label_visibility(self.label_40, self.label_6,str(self.age_list[b]) if self.age_list[b] else '')
+        self.set_label_visibility(self.label_41,self.label_7, self.dob_list[b])
+        self.set_label_visibility(self.label_44,self.label_10, self.mobile_list[b])
+        self.set_label_visibility(self.label_45,self.label_11, self.aadhar_list[b])
+        self.set_label_visibility(self.label_46,self.label_12, self.pan_list[b])
+        self.set_label_visibility(self.label_47,self.label_13, self.city_list[b])
+        self.set_label_visibility(self.label_48,self.label_14, self.email_user_list[b])
+        self.set_label_visibility(self.label_49, self.label_15,bool(self.last_confirm_list[b]))
+        self.set_label_visibility(self.label_50,self.label_16, self.mobile_check_list[b])
+        self.set_label_visibility(self.label_51,self.label_17, self.mother_tongue_list[b])
+        self.set_label_visibility(self.label_52,self.label_18, self.mother_status_list[b])
+        self.set_label_visibility(self.label_54,self.label_20, self.space_name_list[b])
+        self.set_label_visibility(self.label_61,self.label_27, self.about_list[b])
+        self.set_label_visibility(self.label_63,self.label_29, bool(self.alets_list[b]))
+        self.set_label_visibility(self.label_72,self.label_38, bool(self.terms_list[b]))
+        self.set_label_visibility(self.label_69,self.label_35, self.qualification_list[b])
+        self.set_label_visibility(self.label_62,self.label_28, self.address_type_list[b])
+        self.set_label_visibility(self.label_71,self.label_37, self.street_list[b])
+        self.set_label_visibility(self.label_64,self.label_30, self.build_name_list[b])
+        self.set_label_visibility(self.label_66,self.label_32, self.house_no_list[b])
+        self.set_label_visibility(self.label_65,self.label_31, self.landmark_list[b])
+        self.set_label_visibility(self.label_68,self.label_34, self.pincode_list[b])
+        self.set_label_visibility(self.label_70,self.label_36, self.state_list[b])
+        self.set_label_visibility(self.label_55,self.label_21, self.spouse_number_list[b])
+        self.set_label_visibility(self.label_56,self.label_22, self.company_name_list[b])
+        self.set_label_visibility(self.label_57, self.label_23,self.company_adress_list[b])
+        self.set_label_visibility(self.label_58,self.label_24, self.proffic_list[b])
+        self.set_label_visibility(self.label_59,self.label_25, self.user_type_list[b])
+        self.set_label_visibility(self.label_60,self.label_26, bool(self.approve_list[b]))
+        self.set_label_visibility(self.label_74,self.label_73, self.another_email[b])
+        self.set_label_visibility(self.label_76,self.label_75, self.company_name[b])
+        self.set_label_visibility(self.label_77,self.label_42, self.organization_type[b])
+        self.set_label_visibility(self.label_79,self.label_78, self.employment_type[b])
+        self.set_label_visibility(self.label_81,self.label_80, self.business_no[b])
+        self.set_label_visibility(self.label_83,self.label_82, self.company_landmark[b])
+        self.set_label_visibility(self.label_85,self.label_84, self.company_address[b])
+        self.set_label_visibility(self.label_87,self.label_86, self.annual_salary[b])
+        self.set_label_visibility(self.label_89,self.label_88, self.designation[b])
+        self.set_label_visibility(self.label_91,self.label_90, self.account_name[b])
+        self.set_label_visibility(self.label_93,self.label_92, self.account_type[b])
+        self.set_label_visibility(self.label_95,self.label_94, self.account_number[b])
+        self.set_label_visibility(self.label_97,self.label_96, self.account_bank_branch[b])
+        self.set_label_visibility(self.label_101,self.label_100, self.salary_type[b])
+        self.set_label_visibility(self.label_107, self.label_106,self.father_name[b])
+        self.set_label_visibility(self.label_109,self.label_108, self.father_age[b])
+        self.set_label_visibility(self.label_111,self.label_110, self.mother_name[b])
+        self.set_label_visibility(self.label_113,self.label_112, self.mother_age[b])
+        self.set_label_visibility(self.label_115,self.label_114, self.college_name[b])
+        self.set_label_visibility(self.label_117,self.label_116, self.college_id[b])
+        self.set_label_visibility(self.label_119,self.label_118, self.college_address[b])
+
+        # Set image sources
         self.image_2.source = self.profile[b]
         self.image_3.source = self.aadhaar_photo[b]
         self.image_4.source = self.pan_photo[b]
         self.image_5.source = self.emp_id_proof[b]
         self.image_6.source = self.last_six_month_bank_proof[b]
         self.image_7.source = self.college_proof[b]
+
+  def set_label_visibility(self, label,label_1, data ):
+    if data:
+        label.text = data
+        label.visible = True
+        label_1.visible = True
+    else:
+        label.visible = False
+        label_1.visible = False
+      
+        # self.label_8.text = self.name_list[b]
+        # self.label_9.text = bool(self.status_list[b])
+        # self.label_39.text= self.gender_list[b]
+        # self.label_40.text = (self.age_list[b])
+        # self.label_41.text = self.dob_list[b]
+        # self.label_44.text = self.mobile_list[b]
+        # self.label_45.text = self.aadhar_list[b]
+        # self.label_46.text = self.pan_list[b]
+        # self.label_47.text = self.city_list[b]
+        # self.label_48.text = self.email_user_list[b]
+        # self.label_49.text = bool(self.last_confirm_list[b])
+        # self.label_50.text = self.mobile_check_list[b]
+        # self.label_51.text = self.mother_tongue_list[b]
+        # self.label_52.text = self.mother_status_list[b]
+        # # self.label_53.text = self.date_marrige_list[b]
+        # self.label_54.text = self.space_name_list[b]
+        # self.label_61.text = self.about_list[b]
+        # self.label_63.text = bool(self.alets_list[b])
+        # self.label_72.text = bool(self.terms_list[b])
+        # self.label_69.text = self.qualification_list[b]
+        # self.label_62.text = self.address_type_list[b]
+        # self.label_71.text = self.street_list[b]
+        # self.label_64.text = self.build_name_list[b]
+        # self.label_66.text = self.house_no_list[b]
+        # self.label_65.text = self.landmark_list[b]
+        # self.label_68.text = self.pincode_list[b]
+        # self.label_70.text = self.state_list[b]
+        # self.label_55.text = self.spouse_number_list[b]
+        # self.label_56.text = self.company_name_list[b]
+        # self.label_57.text = self.company_adress_list[b]
+        # self.label_58.text =  self.proffic_list[b]
+        # self.label_59.text = self.user_type_list[b]
+        # self.label_60.text = bool(self.approve_list [b])
+        # self.label_74.text = self.another_email[b]
+        # self.label_76.text = self.company_name[b]
+        # self.label_77.text = self.organization_type[b]
+        # self.label_79.text = self.employment_type[b]
+        # self.label_81.text = self.business_no[b]
+        # self.label_83.text = self.company_landmark[b]
+        # self.label_85.text = self.company_address[b]
+        # self.label_87.text = self.annual_salary[b]
+        # self.label_89.text = self.designation[b]
+        # self.label_91.text = self.account_name[b]
+        # self.label_93.text = self.account_type[b]
+        # self.label_95.text = self.account_number[b]
+        # self.label_97.text = self.account_bank_branch[b]
+        # # self.label_99.text = self.ifsc_code[b]
+        # self.label_101.text = self.salary_type[b]
+        # #self.label_103.text = self.select_bank[b]
+        # # self.label_105.text = self.net_bank[b]
+        # self.label_107.text = self.father_name[b]
+        # self.label_109.text = self.father_age[b]
+        # self.label_111.text = self.mother_name[b]
+        # self.label_113.text = self.mother_age[b]
+        # self.label_115.text = self.college_name[b]
+        # self.label_117.text = self.college_id[b]
+        # self.label_119.text = self.college_address[b]
+        # # self.label_121.text = self.running_loan[b]
+        # self.image_2.source = self.profile[b]
+        # self.image_3.source = self.aadhaar_photo[b]
+        # self.image_4.source = self.pan_photo[b]
+        # self.image_5.source = self.emp_id_proof[b]
+        # self.image_6.source = self.last_six_month_bank_proof[b]
+        # self.image_7.source = self.college_proof[b]
 
   def button_2_click(self, **event_args):
     """This method is called when the button is clicked"""
