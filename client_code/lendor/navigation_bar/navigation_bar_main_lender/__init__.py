b55b577a81f120cf9127b97fa1387f746f6946bc
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-05-09 07:34:27 +0000

    Edited lendor.navigation_bar.navigation_bar_main_lender, lendor.navigation_bar.Navigation_bar_lender_dashbord and lendor.dashboard.dashboard_report_a_problem

--- client_code/lendor/navigation_bar/navigation_bar_main_lender/__init__.py
+++ client_code/lendor/navigation_bar/navigation_bar_main_lender/__init__.py
@@ -7,7 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ...bank_users.main_form import main_form_module
+from ....bank_users.main_form import main_form_module
 
 class navigation_bar_main_lender(navigation_bar_main_lenderTemplate):
   def __init__(self, **properties):
