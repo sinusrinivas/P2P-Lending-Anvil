3ea4c338bb57792a798016f01ce0c8fe9f4ef37b
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-05-09 06:21:15 +0000

    Edited borrower.borrower_registration_forms.borrwer_registration_navigation_bar

--- client_code/borrower/borrower_registration_forms/borrwer_registration_navigation_bar/__init__.py
+++ client_code/borrower/borrower_registration_forms/borrwer_registration_navigation_bar/__init__.py
@@ -15,3 +15,7 @@ class borrwer_registration_navigation_bar(borrwer_registration_navigation_barTem
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def borrower_dashboard_home_linkhome_borrower_registration_button_copy_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    
