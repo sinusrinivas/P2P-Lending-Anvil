18b65fce433e6b409bbaad1d06cd7020540aa32f
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-05-15 12:03:50 +0000

    Edited admin.dashboard.admin_management, admin.dashboard.loan_management, admin.dashboard.manage_bessem, admin.dashboard.manage_cms and 8 more

--- client_code/admin/dashboard/manage_settings/__init__.py
+++ client_code/admin/dashboard/manage_settings/__init__.py
@@ -34,3 +34,7 @@ class manage_settings(manage_settingsTemplate):
   def button_4_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('admin.dashboard.manage_settings.manage_facts')
+
+  def image_4_copy_5_mouse_up(self, x, y, button, **event_args):
+    """This method is called when a mouse button is released on this component"""
+    open_form('admin.dashboard.manage_settings.manage_membership')
