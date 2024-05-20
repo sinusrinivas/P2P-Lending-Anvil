376e9df2aa2179cc05fcde9751eb361011a9b43d
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-05-20 06:55:23 +0000

    Created admin.dashboard.loan_management.manage_loan_details.loan_settings and admin.dashboard.loan_management.manage_loan_details; Edited admin.dashboard.loan_management.approved_loans.ItemTemplate31, admin.dashboard.loan_management.closed_loans.ItemTemplate39, admin.dashboard.loan_management.default_loans.ItemTemplate32, admin.dashboard.loan_management.lapsed_loans.ItemTemplate23 and 7 more

---
+++ client_code/admin/dashboard/loan_management/manage_loan_details/loan_settings/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import loan_settingsTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class loan_settings(loan_settingsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def save_button(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
