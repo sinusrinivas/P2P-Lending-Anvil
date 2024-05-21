34dd1ea3f9d2f8952d34bd6e63fbc420ca855331
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-05-21 06:00:36 +0000

    Edited lendor.dashboard.RowTemplate30

--- client_code/lendor/dashboard/RowTemplate30/__init__.py
+++ client_code/lendor/dashboard/RowTemplate30/__init__.py
@@ -15,3 +15,12 @@ class RowTemplate30(RowTemplate30Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    selcted_row=self.item
+    open_form('lendor.dashboard.view_details_1_copy',selected_row=selcted_row)
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
