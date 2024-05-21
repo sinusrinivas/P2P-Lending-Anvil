b4f1a7995e55151f181675c3b8124f771a806d6b
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-05-21 09:45:43 +0000

    Edited lendor.dashboard.RowTemplate30, lendor.dashboard.RowTemplate13 and lendor.dashboard

--- client_code/lendor/dashboard/RowTemplate30/__init__.py
+++ client_code/lendor/dashboard/RowTemplate30/__init__.py
@@ -7,12 +7,13 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+# from .. import main_form_module
 
 class RowTemplate30(RowTemplate30Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    # self.user_id = main_form_module.userId
 
     # Any code you write here will run before the form opens.
 
@@ -21,6 +22,6 @@ class RowTemplate30(RowTemplate30Template):
     open_form('lendor.dashboard.view_details_1_copy',selected_row=selcted_row)
 
   def link_2_click(self, **event_args):
-      borrower_customer_id = self.userId
-      open_form("lendor.dashboard.view_borrower_loan_request.Borr_loan_request", selected_row=borrower_customer_id)
+      selcted_row=self.item
+      open_form("lendor.dashboard.view_borrower_loan_request.Borr_loan_request", selected_row=selcted_row)
     
