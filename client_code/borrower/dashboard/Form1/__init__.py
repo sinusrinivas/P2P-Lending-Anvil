97398150132c8f220801f702e69a078fa2045cc2
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-05-24 07:45:58 +0000

    Edited settings and borrower.dashboard.Form1

--- client_code/borrower/dashboard/Form1/__init__.py
+++ client_code/borrower/dashboard/Form1/__init__.py
@@ -12,6 +12,12 @@ from anvil.tables import app_tables
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    self.init_components(**properties)     
+    self.content_panel_copy_2_copy_2.width = '0%'
+    
 
     # Any code you write here will run before the form opens.
+
+  def button_1_copy_click(self, **event_args):
+    self.content_panel_copy_2_copy_2.visible = True
+    self.content_panel_copy_2_copy_2.width = '100%'
