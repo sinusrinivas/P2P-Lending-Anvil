c45fc12e758f98686777b47de5f2d8eb709b3c45
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-05-28 04:35:28 +0000

    Edited settings and lendor.dashboard.my_returns

--- client_code/lendor/dashboard/my_returns/__init__.py
+++ client_code/lendor/dashboard/my_returns/__init__.py
@@ -16,3 +16,16 @@ class my_returns(my_returnsTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.create_bar_chart()
+
+  def create_bar_chart(self):
+    # Sample data
+    categories = ['A', 'B', 'C', 'D', 'E']
+    values = [10, 15, 13, 17, 20]
+
+    # Create a bar chart
+    trace = go.Bar(x=categories, y=values, name='Bar Chart')
+    layout = go.Layout(title='Sample Bar Chart', xaxis=dict(title='Category'), yaxis=dict(title='Value'))
+    fig = go.Figure(data=[trace], layout=layout)
+    # Set the plot in the Plot component
+    self.plot_1.data = fig
