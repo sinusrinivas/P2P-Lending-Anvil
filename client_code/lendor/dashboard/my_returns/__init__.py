223b8abcb6ca9d322c6821253ebeafb830ffe947
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-05-28 04:16:55 +0000

    Created lendor.dashboard.my_returns

---
+++ client_code/lendor/dashboard/my_returns/__init__.py
@@ -0,0 +1,18 @@
+from ._anvil_designer import my_returnsTemplate
+from anvil import *
+import plotly.graph_objects as go
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class my_returns(my_returnsTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
