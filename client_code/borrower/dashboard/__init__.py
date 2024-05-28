90f7263491700c70c970a1f0836eff224e42d9ea
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-05-28 11:04:49 +0000

    Created borrower.dashboard.RowTemplate7, borrower.dashboard.application_tracker, borrower.dashboard.application_tracker.ItemTemplate68, borrower.dashboard.application_tracker.ItemTemplate69 and 44 more; Edited borrower.dashboard, bank_users.user_form.user_module and borrower.dashboard1; Deleted borrower.dashboard1.RowTemplate7, borrower.dashboard1.application_tracker, borrower.dashboard1.application_tracker.ItemTemplate68, borrower.dashboard1.application_tracker.ItemTemplate69 and 44 more

--- client_code/borrower/dashboard/__init__.py
+++ client_code/borrower/dashboard/__init__.py
@@ -5,8 +5,8 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ....bank_users.main_form import main_form_module
-from ....bank_users.user_form import user_form
+from ...bank_users.main_form import main_form_module
+from ...bank_users.user_form import user_form
 
 class dashboard(dashboardTemplate):
   def __init__(self, **properties):
