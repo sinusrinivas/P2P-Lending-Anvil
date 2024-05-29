4a42dc2112bd975441922c9ad80155d9659d671f
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-05-29 04:36:51 +0000

    Created borrower/dashboard/ new loan request.png, borrower/dashboard/application-tracking.png, borrower/dashboard/loan-extension.png, borrower/dashboard/portfolio.png and 4 more; Edited settings and lendor.Form1; Deleted borrower/dashboard/application tracker.png, borrower/dashboard/banking.png, borrower/dashboard/borrower_partpayment.png, borrower/dashboard/borrower_portfolio.png and 13 more

--- client_code/lendor/Form1/__init__.py
+++ client_code/lendor/Form1/__init__.py
@@ -15,3 +15,26 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def home_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('lendor.dashboard')
+
+  def contact_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form("lendor.dashboard.dasboard_contact")
+
+  def about_main_form_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('lendor.dashboard.dasboard_about')
+
+  def link_11_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('lendor.dashboard.dashboard_report_a_problem')
+
+  def link_9_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    customer_id = self.user_id
+    email = self.email
+    anvil.server.call('fetch_profile_data_and_insert', email, customer_id)
+    open_form("wallet.wallet")
