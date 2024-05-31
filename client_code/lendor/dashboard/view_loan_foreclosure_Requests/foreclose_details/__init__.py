896dc679cd77b1d7df7d471aea7ea8d772c7f4e4
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-05-31 10:53:03 +0000

    Edited lendor.dashboard.view_loan_foreclosure_Requests.foreclose_details

--- client_code/lendor/dashboard/view_loan_foreclosure_Requests/foreclose_details/__init__.py
+++ client_code/lendor/dashboard/view_loan_foreclosure_Requests/foreclose_details/__init__.py
@@ -7,6 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import datetime ,timedelta
 
 
 class foreclose_details(foreclose_detailsTemplate):
