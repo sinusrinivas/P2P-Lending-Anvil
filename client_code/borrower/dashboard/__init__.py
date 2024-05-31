614b1a7ba2f246931478f85a8b777fab5cbdf007
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-05-31 10:33:38 +0000

    Edited borrower.dashboard

--- client_code/borrower/dashboard/__init__.py
+++ client_code/borrower/dashboard/__init__.py
@@ -16,9 +16,9 @@ class dashboard(dashboardTemplate):
     self.init_components(**properties)
     
     self.email = main_form_module.email
-    self.user_Id = main_form_module.userId
+    self.user_id = main_form_module.userId
     self.email = self.email
-    user_id = self.user_Id
+    user_id = self.user_id
     self.populate_loan_history()
 
     wallet = app_tables.fin_wallet.get(customer_id=user_id)
