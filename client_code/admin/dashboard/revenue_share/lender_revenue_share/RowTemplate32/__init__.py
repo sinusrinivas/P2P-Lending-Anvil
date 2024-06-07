0b8b0d606f90c3ac354553f1c9b385da79c29dee
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-06-07 11:30:42 +0000

    Created admin.dashboard.revenue_share.lender_revenue_share.RowTemplate32; Edited admin.dashboard.revenue_share.RowTemplate31 and admin.dashboard.revenue_share.lender_revenue_share

--- client_code/admin/dashboard/revenue_share/lender_revenue_share/RowTemplate32/__init__.py
+++ client_code/admin/dashboard/revenue_share/lender_revenue_share/RowTemplate32/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate31Template
+from ._anvil_designer import RowTemplate32Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,13 +9,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class RowTemplate31(RowTemplate31Template):
+class RowTemplate32(RowTemplate32Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def link_1_click(self, **event_args):
-    customer_id = self.link_1.text
-    open_form('admin.dashboard.revenue_share.lender_revenue_share',customer_id)
