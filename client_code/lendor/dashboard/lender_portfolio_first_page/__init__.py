4953feaa24542cb9c422a18d3f735f98d65b7603
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-07 05:22:58 +0000

    Created lendor.dashboard.lender_portfolio_first_page

---
+++ client_code/lendor/dashboard/lender_portfolio_first_page/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import lender_portfolio_first_pageTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class lender_portfolio_first_page(lender_portfolio_first_pageTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
