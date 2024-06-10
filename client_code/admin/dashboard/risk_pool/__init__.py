bfb80c2651573bebb33414ef11b3b59fa44f6585
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-06-10 03:58:13 +0000

    Created admin.dashboard.risk_pool.ItemTemplate105; Edited settings and admin.dashboard.risk_pool

--- client_code/admin/dashboard/risk_pool/__init__.py
+++ client_code/admin/dashboard/risk_pool/__init__.py
@@ -12,12 +12,82 @@ class risk_pool(risk_poolTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.items = []
 
-    # Any code you write here will run before the form opens.
+  def button_low_risk_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    lapsed_settings = app_tables.fin_loan_settings.get(loans="lapsed fee")
+    min_days = lapsed_settings['minimum_days']
+    max_days = lapsed_settings['maximum_days']
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('admin.dashboard')
+    # Fetch rows from EMI table where days left is within the range
+    emi_rows = app_tables.fin_emi_table.search()
+    filtered_rows = [row for row in emi_rows if min_days <= row['days_left'] <= max_days]
 
-  def button_1_copy_3_click(self, **event_args):
-    open_form('admin.dashboard.manage_settings')
+    # Set the filtered rows to the repeating panel
+    result = []
+    for row in filtered_rows:
+      loan_details = app_tables.fin_loan_details.get(loan_id=row['loan_id'])
+      if loan_details:
+        combined_data = {
+          'emi_number': row['emi_number'],
+          'borrower_customer_id': row['borrower_customer_id'],
+          'product_name': loan_details['product_name'],
+          'borrower_full_name': loan_details['borrower_full_name']
+        }
+        result.append(combined_data)
+    
+    # Set the combined data to the repeating panel
+    self.repeating_panel_1.items = result
+
+  def button_medium_risk_click(self, **event_args):
+      """This method is called when the button is clicked"""
+      lapsed_settings = app_tables.fin_loan_settings.get(loans="default fee")
+      min_days = lapsed_settings['minimum_days']
+      max_days = lapsed_settings['maximum_days']
+  
+      # Fetch rows from EMI table where days left is within the range
+      emi_rows = app_tables.fin_emi_table.search()
+      filtered_rows = [row for row in emi_rows if min_days <= row['days_left'] <= max_days]
+  
+      # Fetch additional details from the loan details table
+      result = []
+      for row in filtered_rows:
+        loan_details = app_tables.fin_loan_details.get(loan_id=row['loan_id'])
+        if loan_details:
+          combined_data = {
+            'emi_number': row['emi_number'],
+            'borrower_customer_id': row['borrower_customer_id'],
+            'product_name': loan_details['product_name'],
+            'borrower_full_name': loan_details['borrower_full_name']
+          }
+          result.append(combined_data)
+      
+      # Set the combined data to the repeating panel
+      self.repeating_panel_1.items = result
+
+  def button_high_risk_click(self, **event_args):
+      """This method is called when the button is clicked"""
+      lapsed_settings = app_tables.fin_loan_settings.get(loans="NPA fee")
+      min_days = lapsed_settings['minimum_days']
+      max_days = lapsed_settings['maximum_days']
+  
+      # Fetch rows from EMI table where days left is within the range
+      emi_rows = app_tables.fin_emi_table.search()
+      filtered_rows = [row for row in emi_rows if min_days <= row['days_left'] <= max_days]
+  
+      # Fetch additional details from the loan details table
+      result = []
+      for row in filtered_rows:
+        loan_details = app_tables.fin_loan_details.get(loan_id=row['loan_id'])
+        if loan_details:
+          combined_data = {
+            'emi_number': row['emi_number'],
+            'borrower_customer_id': row['borrower_customer_id'],
+            'product_name': loan_details['product_name'],
+            'borrower_full_name': loan_details['borrower_full_name']
+          }
+          result.append(combined_data)
+      
+      # Set the combined data to the repeating panel
+      self.repeating_panel_1.items = result
