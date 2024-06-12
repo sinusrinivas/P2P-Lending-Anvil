e85faf1e664b145724d3d7c9a83ff64956232e00
sinusrinivas861@gmail.com <sinusrinivas861@gmail.com>
2024-06-12 07:09:32 +0000

    Edited admin.dashboard.customer_management.managing_customer_profile and admin.dashboard.customer_management.managing_customer_profile.ItemTemplate107

--- client_code/admin/dashboard/customer_management/managing_customer_profile/ItemTemplate107/__init__.py
+++ client_code/admin/dashboard/customer_management/managing_customer_profile/ItemTemplate107/__init__.py
@@ -15,3 +15,18 @@ class ItemTemplate107(ItemTemplate107Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+  # Add a Label component for the name
+    self.name_label = Label(text="Name", align="left", bold=True)
+    self.add_component(self.name_label)
+    
+    # Add a Label component for the email
+    self.email_label = Label(text="Email", align="left", bold=True)
+    self.add_component(self.email_label)
+    
+    # Add a Label component for the alternate email
+    self.alt_email_label = Label(text="Alternate Email", align="left", bold=True)
+    self.add_component(self.alt_email_label)
+    
+    # Add an Image component for the photo
+    self.photo = Image(source=None)
+    self.add_component(self.photo)
--- client_code/admin/dashboard/customer_management/managing_customer_profile/__init__.py
+++ client_code/admin/dashboard/customer_management/managing_customer_profile/__init__.py
@@ -14,8 +14,26 @@ class managing_customer_profile(managing_customer_profileTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
 
+   # Any code you write here will run before the form opens.
+    self.load_user_profile()
+  
+  def load_user_profile(self):
+    """Fetch user profile details from fin_user_profile table and display in the form"""
+    # Assuming there's a method to get the user profile
+    # For example, get the profile of the currently logged-in user
+    user = anvil.users.get_user()
+    if user:
+      user_profile = app_tables.fin_user_profile.get(user=user)
+      if user_profile:
+        self.label_1.text = user_profile['full_name']
+        self.email_label.text = user_profile['email_user']
+        self.alt_email_label.text = user_profile['another_email']
+        self.photo.image = user_profile['user_photo']
+
+  
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('admin.dashboard.customer_management')
+
+
