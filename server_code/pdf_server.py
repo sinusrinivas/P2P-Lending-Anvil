ca24a593a5e9ba1cf7850181b701bb6db05ed342
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-12 07:14:33 +0000

    Edited pdf_server and lendor.dashboard.lender_portfolio

--- server_code/pdf_server.py
+++ server_code/pdf_server.py
@@ -12,9 +12,9 @@ import anvil.pdf
 @anvil.server.callable
 def create_pdf(name, image_source,selected_row):    
     # Your PDF creation logic here
-    pdf = anvil.pdf.render_form("lendor.dashboard.lender_portfolio",selected_row = selected_row)  
+    pdf = anvil.pdf.render_form("lendor.dashboard.lender_portfolio.content",selected_row = selected_row)  
     return pdf
-
+     
 @anvil.server.callable()
 def create_pdf1(name, image_source, selected_row):
     # A4 size in points (width, height) is (595, 842), for landscape swap to (842, 595)
