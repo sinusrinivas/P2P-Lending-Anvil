2b6cb9c294fbd49b18654a87371aba34b13563af
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-17 05:50:23 +0000

    Edited admin.dashboard.accounting.track_loan_disbursement.RowTemplate41 and admin.dashboard.accounting.track_loan_disbursement.view_details

--- client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
+++ client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
@@ -10,8 +10,17 @@ from anvil.tables import app_tables
 
 
 class view_details(view_detailsTemplate):
-  def __init__(self, **properties):
+  def __init__(self, selected_row, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.borrower_full_name.text = selected_row['borrower_full_name'],
+    self.borrower_email.text = selected_row['borrower_email_id'],
+    self.name.text = selected_row['lender_full_name'],
+    self.lender_email.text = selected_row['lender_email_id'],
+    self.interest.text = selected_row['interest_rate'],
+    self.loan_amount.text = selected_row['loan_amount'],
+    self.status.text = selected_row['loan_updated_status'],
+    self.borrower_full_name.text = selected_row['borrower_full_name'],
+    self.borrower_full_name.text = selected_row['borrower_full_name'],
