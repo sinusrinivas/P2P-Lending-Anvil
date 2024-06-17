f91aefac68c0194a0a2db6952990997594c36e9c
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-17 06:11:29 +0000

    Edited admin.dashboard.accounting.track_loan_disbursement.RowTemplate41, settings and admin.dashboard.accounting.track_loan_disbursement.view_details

--- client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
+++ client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
@@ -13,14 +13,17 @@ class view_details(view_detailsTemplate):
   def __init__(self, selected_row, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.selected_row = selected_row
 
     # Any code you write here will run before the form opens.
-    self.borrower_full_name.text = selected_row['borrower_full_name'],
-    self.borrower_email.text = selected_row['borrower_email_id'],
-    self.name.text = selected_row['lender_full_name'],
-    self.lender_email.text = selected_row['lender_email_id'],
-    self.interest.text = selected_row['interest_rate'],
-    self.loan_amount.text = selected_row['loan_amount'],
-    self.status.text = selected_row['loan_updated_status'],
-    self.borrower_full_name.text = selected_row['borrower_full_name'],
-    self.borrower_full_name.text = selected_row['borrower_full_name'],
+    self.borrower_full_name.text = selected_row.get('borrower_full_name', 'N/A')
+    self.borrower_email.text = selected_row.get('borrower_email_id', 'N/A')
+    self.name.text = selected_row.get('lender_full_name', 'N/A')
+    self.lender_email.text = selected_row.get('lender_email_id', 'N/A')
+    self.interest.text = selected_row.get('interest_rate', 'N/A')
+    self.loan_amount.text = selected_row.get('loan_amount', 'N/A')
+    self.status.text = selected_row.get('loan_updated_status', 'N/A')
+    self.repay_amount.text = selected_row.get('total_repayment_amount', 'N/A')
+    self.membership.text = selected_row.get('membership_type', 'N/A')
+    self.emi.text = selected_row.get('emi_payment_type', 'N/A')
+    self.product_name.text = selected_row.get('product_name', 'N/A')
