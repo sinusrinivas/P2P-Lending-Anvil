6de494652b4a2096798777703e1b048decec10f2
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-17 06:46:02 +0000

    Edited admin.dashboard.accounting.track_loan_disbursement.RowTemplate41, admin.dashboard.accounting.track_loan_disbursement and admin.dashboard.accounting.track_loan_disbursement.view_details

--- client_code/admin/dashboard/accounting/track_loan_disbursement/__init__.py
+++ client_code/admin/dashboard/accounting/track_loan_disbursement/__init__.py
@@ -1,3 +1,45 @@
+# from ._anvil_designer import track_loan_disbursementTemplate
+# from anvil import *
+# import anvil.server
+# import anvil.google.auth, anvil.google.drive
+# from anvil.google.drive import app_files
+# import anvil.users
+# import anvil.tables as tables
+# import anvil.tables.query as q
+# from anvil.tables import app_tables
+
+
+# class track_loan_disbursement(track_loan_disbursementTemplate):
+#   def __init__(self, **properties):
+#     # Set Form properties and Data Bindings.
+#     self.init_components(**properties)
+
+#     # Any code you write here will run before the form opens.
+#     self.result = app_tables.fin_loan_details.search()
+#     if not self.result:
+#         Notification("No Data Available Here!").show()
+#     else:
+#         self.result = [{'borrower_full_name': i['borrower_full_name'],
+#                         'borrower_email_id': i['borrower_email_id'] if 'borrower_email_id' in i else 'N/A',
+#                         'lender_full_name': i['lender_full_name'] if 'lender_full_name' in i else 'N/A',
+#                         'lender_email_id': i['lender_email_id'] if 'lender_email_id' in i else 'N/A',
+#                         'interest_rate': i['interest_rate'],
+#                         'loan_amount': i['loan_amount'],
+#                         'loan_updated_status': i['loan_updated_status'],
+#                         'loan_id': i['loan_id'],
+#                         'total_repayment_amount': i['total_repayment_amount'] if 'total_repayment_amount' in i else 'N/A',
+#                         'membership_type': i['membership_type'] if 'membership_type' in i else 'N/A',
+#                         'emi_payment_type': i['emi_payment_type'] if 'emi_payment_type' in i else 'N/A',
+#                         'product_name': i['product_name'] if 'product_name' in i else 'N/A'
+#                        }
+#                        for i in self.result if i['loan_updated_status'] in ["disbursed", "approved"]]
+
+#         if not self.result:
+#             Notification("No Loans with status 'disbursed' or 'approved' found!").show()
+#         else:
+#             self.repeating_panel_1.items = self.result
+
+
 from ._anvil_designer import track_loan_disbursementTemplate
 from anvil import *
 import anvil.server
@@ -19,13 +61,7 @@ class track_loan_disbursement(track_loan_disbursementTemplate):
     if not self.result:
         Notification("No Data Available Here!").show()
     else:
-        self.result = [{'name': i['borrower_full_name'],
-                        'loan_amount': i['loan_amount'],
-                        'interest': i['interest_rate'],
-                        'loan_status': i['loan_updated_status'],
-                        'loan_id': i['loan_id']                        
-                       }
-                       for i in self.result if i['loan_updated_status'] in ["disbursed", "approved"]]
+        self.result = [i for i in self.result if i['loan_updated_status'] in ["disbursed", "approved"]]
 
         if not self.result:
             Notification("No Loans with status 'disbursed' or 'approved' found!").show()
--- client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
+++ client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
@@ -13,17 +13,20 @@ class view_details(view_detailsTemplate):
   def __init__(self, selected_row, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
+    # Store the selected row
     self.selected_row = selected_row
+    print(self.se)
 
     # Any code you write here will run before the form opens.
-    self.borrower_full_name.text = selected_row.get('borrower_full_name', 'N/A')
-    self.borrower_email.text = selected_row.get('borrower_email_id', 'N/A')
-    self.name.text = selected_row.get('lender_full_name', 'N/A')
-    self.lender_email.text = selected_row.get('lender_email_id', 'N/A')
-    self.interest.text = selected_row.get('interest_rate', 'N/A')
-    self.loan_amount.text = selected_row.get('loan_amount', 'N/A')
-    self.status.text = selected_row.get('loan_updated_status', 'N/A')
-    self.repay_amount.text = selected_row.get('total_repayment_amount', 'N/A')
-    self.membership.text = selected_row.get('membership_type', 'N/A')
-    self.emi.text = selected_row.get('emi_payment_type', 'N/A')
-    self.product_name.text = selected_row.get('product_name', 'N/A')
+    self.borrower_full_name.text = selected_row['borrower_full_name'] if 'borrower_full_name' in selected_row else 'N/A'
+    self.borrower_email.text = selected_row['borrower_email_id'] if 'borrower_email_id' in selected_row else 'N/A'
+    self.name.text = selected_row['lender_full_name'] if 'lender_full_name' in selected_row else 'N/A'
+    self.lender_email.text = selected_row['lender_email_id'] if 'lender_email_id' in selected_row else 'N/A'
+    self.interest.text = selected_row['interest_rate'] if 'interest_rate' in selected_row else 'N/A'
+    self.loan_amount.text = selected_row['loan_amount'] if 'loan_amount' in selected_row else 'N/A'
+    self.status.text = selected_row['loan_updated_status'] if 'loan_updated_status' in selected_row else 'N/A'
+    self.repay_amount.text = selected_row['total_repayment_amount'] if 'total_repayment_amount' in selected_row else 'N/A'
+    self.membership.text = selected_row['membership_type'] if 'membership_type' in selected_row else 'N/A'
+    self.emi.text = selected_row['emi_payment_type'] if 'emi_payment_type' in selected_row else 'N/A'
+    self.product_name.text = selected_row['product_name'] if 'product_name' in selected_row else 'N/A'
