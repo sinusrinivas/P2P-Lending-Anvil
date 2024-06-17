11c382b6aeb59e2be62c1442fbc906ceb7e2309e
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-17 05:08:57 +0000

    Edited admin.dashboard.accounting.track_loan_disbursement.RowTemplate41 and admin.dashboard.accounting.track_loan_disbursement

--- client_code/admin/dashboard/accounting/track_loan_disbursement/__init__.py
+++ client_code/admin/dashboard/accounting/track_loan_disbursement/__init__.py
@@ -15,3 +15,17 @@ class track_loan_disbursement(track_loan_disbursementTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.result = app_tables.fin_loan_details.search()
+    if not self.result:
+        Notification("No Data Available Here!").show()
+    else:
+        self.result = [{'name': i['borrower_full_name'],
+                        'loan_amount': i['loan_amount'],
+                        'interest': i['interest_rate'],
+                        'loan_status': i['loan_updated_status'],
+                        'loan_id': i['loan_id'],
+                        
+                       }
+                       for i in self.result]
+
+        self.repeating_panel_1.items = self.result
