fd53765579c051e6f0456e0eec4685bdd511ae6d
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-17 10:37:45 +0000

    Edited admin_server and admin.dashboard.accounting.track_loan_disbursement.view_details

--- client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
+++ client_code/admin/dashboard/accounting/track_loan_disbursement/view_details/__init__.py
@@ -8,26 +8,26 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-
+import plotly.graph_objects as go
 
 class view_details(view_detailsTemplate):
   def __init__(self, selected_row, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Store the selected row
-    self.selected_row = selected_row
-    print(self.selected_row)
-    print(selected_row['loan_id'])
-   
-    self.borrower_full_name.text = f"{selected_row['borrower_full_name']}"
-    self.borrower_email.text = f"{selected_row['borrower_email_id']}"
-    self.name.text = f"{selected_row['lender_full_name']}"
-    self.lender_email.text = f"{selected_row['lender_email_id']}"
-    self.interest.text = f"{selected_row['interest_rate']}"
-    self.loan_amount.text = f"{selected_row['loan_amount']}"
-    self.status.text = f"{selected_row['loan_updated_status']}"
-    self.repay_amount.text = "{0.2f}.{selected_row['total_repayment_amount']}"
-    self.membership.text = f"{selected_row['membership_type']}"
-    self.emi.text = f"{selected_row['emi_payment_type']}"
-    self.product_name.text = f"{selected_row['product_name']}"
+    self.loan_id = selected_row['loan_id']
+ 
+    loan_data = app_tables.fin_loan_details.get(loan_id=self.loan_id)
+    if loan_data:
+        loan_created = loan_data['borrower_loan_created_timestamp']
+        lender_accepted = loan_data['lender_accepted_timestamp']
+        loan_disbursed = loan_data['loan_disbursed_timestamp']
+        self.create_loan_disbursement_graph(loan_created, lender_accepted, loan_disbursed)
+    else:
+        Notification("No data found for the selected loan ID!").show()
+        
+  def create_loan_disbursement_graph(self, loan_created, lender_accepted, loan_disbursed):
+        # Call server function to get Plotly figure
+        fig = anvil.server.call('create_loan_disbursement_graph', loan_created, lender_accepted, loan_disbursed)
+        
+        # Update Plotly component with the retrieved figure
+        self.plot_1.figure = fig
