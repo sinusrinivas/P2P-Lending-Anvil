41679506bf4d73c1fe9c61ede0296ca6ce926bca
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-06-18 05:33:35 +0000

    Edited settings, admin.dashboard.manage_products.view_product and admin.dashboard.manage_products.view_product.RowTemplate5

--- client_code/admin/dashboard/manage_products/view_product/RowTemplate5/__init__.py
+++ client_code/admin/dashboard/manage_products/view_product/RowTemplate5/__init__.py
@@ -19,3 +19,7 @@ class RowTemplate5(RowTemplate5Template):
     """This method is called when the link is clicked"""
     value_to_pass = self.link_1.text
     open_form('admin.dashboard.manage_products.view_profile',value_to_pass)
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
