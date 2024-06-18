ce995448d3f1c2600127e058d9dd906aa72f1f2b
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-06-18 06:24:06 +0000

    Created admin.dashboard.loan_servicing.handle_collection_process.RowTemplate49 and admin.dashboard.loan_servicing.handle_collection_process; Edited settings

---
+++ client_code/admin/dashboard/loan_servicing/handle_collection_process/__init__.py
@@ -0,0 +1,17 @@
+from ._anvil_designer import handle_collection_processTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class handle_collection_process(handle_collection_processTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
