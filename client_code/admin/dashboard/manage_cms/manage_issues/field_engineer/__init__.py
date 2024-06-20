5346b1d8a0e89918338b56610b236f5b4dd2e6ef
pavankumarmallepoola@gmail.com <pavankumarmallepoola@gmail.com>
2024-06-20 15:24:00 +0000

    Edited admin.dashboard.manage_cms.manage_issues.field_engineer

--- client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/__init__.py
+++ client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/__init__.py
@@ -18,9 +18,13 @@ class field_engineer(field_engineerTemplate):
 
     user_profile=app_tables.fin_user_profile.get(customer_id=self.id)
     self.image_1.source = user_profile['user_photo']
+    self.label_6.text=user_profile['present_address']
     customer_details=app_tables.fin_reported_problems.get(customer_id=self.id)
     self.label_2.text=customer_details['name']
     self.label_4.text=customer_details['mobile_number']
+    self.label_8.text=customer_details['category']
+    self.label_10.text=customer_details['subcategory']
+    self.label_13.text=customer_details['']
     
     
     
