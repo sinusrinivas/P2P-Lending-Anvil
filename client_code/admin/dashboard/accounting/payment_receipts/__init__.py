7ec93c04664bf927ae8358d11c4424cd63a5b5c9
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-20 11:10:35 +0000

    Edited admin.dashboard.accounting.payment_receipts

--- client_code/admin/dashboard/accounting/payment_receipts/__init__.py
+++ client_code/admin/dashboard/accounting/payment_receipts/__init__.py
@@ -36,19 +36,20 @@ class payment_receipts(payment_receiptsTemplate):
         lender_profile = app_tables.fin_user_profile.get(customer_id=loan_details['lender_customer_id'])
       else:
         lender_profile = None
-      
+
+      # 'borrower_photo': borrower_profile['user_photo'] ,
+      self.label_7.text = borrower_profile['full_name'] ,
+      # 'borrower_email_id': borrower_profile['email_user'] if borrower_profile else None,
+      self.label_8.text = borrower_profile['mobile'] if borrower_profile else None,
+      self.label_19.text= borrower_profile['present_address'] if borrower_profile else None,
+      # 'lender_photo': lender_profile['user_photo'] if lender_profile else None,
+      self.la: lender_profile['full_name'] if lender_profile else None,
+      'lender_email_id': lender_profile['email_user'] if lender_profile else None,
+      'lender_mobile': lender_profile['mobile'] if lender_profile else None,
+      'lender_address': lender_profile['present_address'] if lender_profile else None,
       # Append the EMI details to the filtered EMIs list
       filtered_emis.append({
-        'borrower_photo': borrower_profile['user_photo'] if borrower_profile else None,
-        'borrower_full_name': borrower_profile['full_name'] if borrower_profile else None,
-        'borrower_email_id': borrower_profile['email_user'] if borrower_profile else None,
-        'borrower_mobile': borrower_profile['mobile'] if borrower_profile else None,
-        'borrower_address': borrower_profile['present_address'] if borrower_profile else None,
-        'lender_photo': lender_profile['user_photo'] if lender_profile else None,
-        'lender_full_name': lender_profile['full_name'] if lender_profile else None,
-        'lender_email_id': lender_profile['email_user'] if lender_profile else None,
-        'lender_mobile': lender_profile['mobile'] if lender_profile else None,
-        'lender_address': lender_profile['present_address'] if lender_profile else None,
+       
         'amount_paid': emi['amount_paid'],
         
        
@@ -66,4 +67,4 @@ class payment_receipts(payment_receiptsTemplate):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('admin.dashboard.accounting')
+    open_form('admin.dashboard.accounting.payment_receipts')
