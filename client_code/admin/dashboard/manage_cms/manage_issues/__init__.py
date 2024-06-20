eb4001599662da0d5d2f07b82aefa13ecc8181e5
pavankumarmallepoola@gmail.com <pavankumarmallepoola@gmail.com>
2024-06-20 05:17:29 +0000

    Created admin.dashboard.manage_cms.manage_issues.RowTemplate41 and admin.dashboard.manage_cms.manage_issues.field_engineer; Edited settings and admin.dashboard.manage_cms.manage_issues; Deleted admin.dashboard.manage_cms.manage_issues.ItemTemplate82

--- client_code/admin/dashboard/manage_cms/manage_issues/__init__.py
+++ client_code/admin/dashboard/manage_cms/manage_issues/__init__.py
@@ -14,13 +14,14 @@ class manage_issues(manage_issuesTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    items = [row for row in app_tables.fin_reported_problems.search() if not row['status']]
-    self.repeating_panel_1.items = items
+    #items = [row for row in app_tables.fin_reported_problems.search() if row['email']]
+    #self.repeating_panel_1.items = items
         
-    if not items:
-        self.label_no_issues.visible = True
-    else:
-        self.label_no_issues.visible = False
+    #if not items:
+        #self.label_no_issues.visible = True
+    #else:
+        #self.label_no_issues.visible = False
+    self.repeating_panel_1.items = app_tables.fin_reported_problems.search()
 
   def button_1_click(self, **event_args):
     open_form('admin.dashboard.manage_cms')
