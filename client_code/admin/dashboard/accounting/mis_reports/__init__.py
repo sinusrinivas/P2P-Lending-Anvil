92281888b140192baf221c593cfd7bbab7e920b7
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-06-20 09:20:05 +0000

    Edited settings and admin.dashboard.accounting.mis_reports

--- client_code/admin/dashboard/accounting/mis_reports/__init__.py
+++ client_code/admin/dashboard/accounting/mis_reports/__init__.py
@@ -44,6 +44,7 @@ class mis_reports(mis_reportsTemplate):
 
     # Any code you write here will run before the form opens.
     self.plot_data()
+    self.plot_loan_data()
 
   def plot_data(self):
     # Fetch data from tables
@@ -86,11 +87,54 @@ class mis_reports(mis_reportsTemplate):
     # Create the pie chart
     fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent')])
 
-    # Update layout for better appearance
-    fig.update_layout(title_text='Financial Loan Details')
+
+    fig.update_layout(title={'text': 'Financial Loan Details', 'font': {'size': 24, 'color': 'black', 'family': 'Arial', 'bold': True}})
 
     # Embed the plot in the Anvil app
     self.plot_1.figure = fig
+
+  def plot_loan_data(self):
+    # Fetch data from tables
+    loan_details = app_tables.fin_loan_details.search(loan_updated_status=q.any_of('closed loan', 'rejected', 'disbursed loan', 'foreclosure', 'lost opportunities', 'accepted', 'under process'))
+
+    # Calculate metrics
+    no_of_loans_disbursed = len([loan for loan in loan_details if loan['loan_updated_status'] == 'disbursed loan'])
+    no_of_loans_closed = len([loan for loan in loan_details if loan['loan_updated_status'] == 'closed loan'])
+    no_of_loans_rejected = len([loan for loan in loan_details if loan['loan_updated_status'] == 'rejected'])
+    no_of_loans_foreclosed = len([loan for loan in loan_details if loan['loan_updated_status'] == 'foreclosure'])
+    no_of_loans_lost_opportunity = len([loan for loan in loan_details if loan['loan_updated_status'] == 'lost opportunities'])
+    no_of_loans_accepted = len([loan for loan in loan_details if loan['loan_updated_status'] == 'accepted'])
+    no_of_loans_under_process = len([loan for loan in loan_details if loan['loan_updated_status'] == 'under process'])
+    # Data for the pie chart
+    values = [
+        no_of_loans_disbursed,
+        no_of_loans_closed,
+        no_of_loans_rejected,
+        no_of_loans_foreclosed,
+        no_of_loans_lost_opportunity,
+        no_of_loans_accepted,
+        no_of_loans_under_process
+
+    ]
+    labels = [
+        'No of Loans Disbursed: {}'.format(no_of_loans_disbursed),
+        'No of Loans Closed: {}'.format(no_of_loans_closed),
+        'No of Loans Rejected: {}'.format(no_of_loans_rejected),
+        'No of Loans Foreclosed: {}'.format(no_of_loans_foreclosed),
+        'No of Loans Lost Opportunity: {}'.format(no_of_loans_lost_opportunity),
+        'No of Loans Accepted: {}'.format(no_of_loans_accepted),
+        'No of Loans Under Process: {}'.format(no_of_loans_under_process)
+
+    ]
+
+    # Create the pie chart
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+percent')])
+
+    # Update layout for better appearance
+    fig.update_layout(title={'text': 'Financial Details on Loans', 'font': {'size': 24, 'color': 'black', 'family': 'Arial', 'bold': True}})
+
+    # Embed the plot in the Anvil app
+    self.plot_2.figure = fig
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
