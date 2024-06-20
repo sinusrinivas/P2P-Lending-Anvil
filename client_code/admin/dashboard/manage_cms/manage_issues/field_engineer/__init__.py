03451fa7114ef40f058ff2a8264c201f3245aa74
pavankumarmallepoola@gmail.com <pavankumarmallepoola@gmail.com>
2024-06-20 13:55:15 +0000

    Edited settings, admin.dashboard.manage_cms.manage_issues and admin.dashboard.manage_cms.manage_issues.field_engineer

--- client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/__init__.py
+++ client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/__init__.py
@@ -10,14 +10,19 @@ from anvil.tables import app_tables
 
 
 class field_engineer(field_engineerTemplate):
-  def __init__(self,selected_row, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-    self.selected_row = selected_row
-    print(self.selected_row)
-    #cust_id = app_tables.fin_user_profile.search()
+  def __init__(self,main_form_module, **properties):
 
-  #if selected_row in cust
+    self.user_id=main_form_module.userId 
+    self.init_components(**properties)
+
+    user_profile=app_tables.fin_user_profile.get(customer_id=self.user_id)
+    if user_profile: 
+      self.label_2.text=user_profile['full_name']
+      self.label_4.text=user_profile['mobile']
+      self.label_6.text=user_profile['present_address']
+      self.label_8.text=user_profile['date_of_birth']
+      self.city.text=user_profile['city']
+   
     
 
   def button_1_click(self, **event_args):
