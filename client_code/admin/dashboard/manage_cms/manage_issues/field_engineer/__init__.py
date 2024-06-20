de45ef9fe4c3b2ab6f3e28b6c8e023e49cf5116a
pavankumarmallepoola@gmail.com <pavankumarmallepoola@gmail.com>
2024-06-20 12:34:21 +0000

    Edited admin.dashboard.manage_cms.manage_issues.field_engineer

--- client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/__init__.py
+++ client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/__init__.py
@@ -13,17 +13,12 @@ class field_engineer(field_engineerTemplate):
   def __init__(self,selected_row, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.data = app_tables.fin_reported_problems.search()
-    self.address = app_tables.fin_urser_profile.search
-   
-    for i in self.data:
-      if selected_row in self.data(i['email']):
-        self.label_2.text = self.data(i['name'])
-        self.label_4.text = self.data(i['mobile_number'])
-        self.label_6.text = self.data(i['issue_description'])
-        self.label_8.text = 
+    self.selected_row = selected_row
+    print(self.selected_row)
+    #cust_id = app_tables.fin_user_profile.search()
 
-    # Any code you write here will run before the form opens.
+  #if selected_row in cust
+    
 
   def button_1_click(self, **event_args):
     open_form('admin.dashboard.manage_cms.manage_issues')
