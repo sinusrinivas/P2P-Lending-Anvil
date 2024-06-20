f322c7d8084220727c6cb2fb38c191f6a348a286
pavankumarmallepoola@gmail.com <pavankumarmallepoola@gmail.com>
2024-06-20 07:16:23 +0000

    Created admin.dashboard.manage_cms.manage_issues.field_engineer.ItemTemplate82; Edited settings and admin.dashboard.manage_cms.manage_issues.field_engineer

--- client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/ItemTemplate82/__init__.py
+++ client_code/admin/dashboard/manage_cms/manage_issues/field_engineer/ItemTemplate82/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import field_engineerTemplate
+from ._anvil_designer import ItemTemplate82Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,17 +9,9 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class field_engineer(field_engineerTemplate):
+class ItemTemplate82(ItemTemplate82Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    open_form('admin.dashboard.manage_cms.manage_issues')
-
-  def Manage_issues_click(self, **event_args):
-    self.column_panel_1.visible = True
-    self.repeating_panel_1.items = app_tables.fin_field_engineers.search()
-
