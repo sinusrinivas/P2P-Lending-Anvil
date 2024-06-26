83240e5857985d675f0482639404a935cea083d4
sinusrinivas861@gmail.com <sinusrinivas861@gmail.com>
2024-06-26 06:31:43 +0000

    Edited admin.dashboard.loan_management.application_intake.All_loans.RowTemplate62, admin.dashboard.loan_management.application_intake.Approved_loans, admin.dashboard.loan_management.application_intake.Approved_loans.RowTemplate64, admin.dashboard.loan_management.application_intake.Rejected_loans and 5 more

--- client_code/admin/dashboard/loan_management/application_intake/Rejected_loans/__init__.py
+++ client_code/admin/dashboard/loan_management/application_intake/Rejected_loans/__init__.py
@@ -15,3 +15,30 @@ class Rejected_loans(Rejected_loansTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    loan_details = app_tables.fin_loan_details.search()
+
+    # Filter loan details for loans with 'disbursed' status
+    filtered_loans = [loan for loan in loan_details if loan['loan_updated_status'] == 'rejected']
+
+        # Prepare data for displaying in UI
+    combined_data = []
+    for loan in filtered_loans:
+            combined_data.append({
+                'lender_email_id': loan['lender_email_id'],
+                'lender_full_name': loan['lender_full_name'],
+                'lender_customer_id': loan['lender_customer_id'],
+                'membership_type': loan['membership_type'],
+                'product_name': loan['product_name'],
+                'loan_amount': loan['loan_amount'],
+                'loan_updated_status': loan['loan_updated_status'],
+                'borrower_full_name': loan['borrower_full_name'],
+                'borrower_customer_id': loan['borrower_customer_id'],
+                'borrower_email_id': loan['borrower_email_id']
+            })
+        
+        # Bind data to the DataGrid component
+    self.repeating_panel_1.items = combined_data
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('admin.dashboard.loan_management.application_intake')
