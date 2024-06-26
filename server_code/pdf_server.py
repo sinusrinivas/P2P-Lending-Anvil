fc1561dfc80cb7709e41bc2e0e8c9dbcce9617ff
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-06-26 09:10:34 +0000

    Edited admin.dashboard.accounting.mis_reports and pdf_server

--- server_code/pdf_server.py
+++ server_code/pdf_server.py
@@ -43,6 +43,15 @@ def create_pdf_of_lender_portfolio(content_panel):
     
     return pdf
   
+
+# code to generate pdf of MIS reports
+@anvil.server.callable
+def create_pdf_of_mis_reports(content_panel):
+    # Render the panel to a PDF
+    pdf = anvil.pdf.PDFRenderer(landscape=True).render_form(content_panel)
+    
+    return pdf
+  
 # @anvil.server.callable
 # def create_pdf(name, image_source,selected_row):    
 #     # Your PDF creation logic here
