49ac0dc115287ff7ee70e1d4424872676c259abf
dinakaryaramati95355@gmail.com <dinakaryaramati95355@gmail.com>
2024-06-27 05:34:12 +0000

    Created admin.dashboard.accounting.mis_reports.behavioural_report.defaulters.RowTemplate67; Edited admin.dashboard.accounting.mis_reports.behavioural_report.defaulters

--- client_code/admin/dashboard/accounting/mis_reports/behavioural_report/defaulters/__init__.py
+++ client_code/admin/dashboard/accounting/mis_reports/behavioural_report/defaulters/__init__.py
@@ -10,27 +10,28 @@ from anvil.tables import app_tables
 
 
 class defaulters(defaultersTemplate):
-  def __init__(self, **properties):
-
+    def __init__(self, **properties):
         self.init_components(**properties)
         self.defaulters_data()
 
-  def defaulters_data(self):
-        # Fetch data from the fin_loan_details table where loan_updated_status is 'extension'
-        loan_details = app_tables.fin_emi_table.search()
+    def defaulters_data(self):
+        # Fetch data from the fin_emi_table where default_fee is greater than or equal to 3
+        emi_details = app_tables.fin_emi_table.search(default_fee=q.gte(3))
         
-
         result = []
-        for borrower_id, details in borrower_loans.items():
-            if len(details['loans']) >= 2:
-                result.append({
-                    'borrower_customer_id': borrower_id,
-                    'borrower_full_name': details['borrower_full_name'],
-                    'borrower_email_id': details['borrower_email_id'],
-                    'mobile_no': details['mobile_no'],
-                    'loans_count': len(details['loans']),
-                    'product_names': ", ".join(details['product_names'])  # Convert set to a comma-separated string
-                })
+        for emi in emi_details:
+            borrower_id = emi['borrower_customer_id']
+            user_profile = app_tables.fin_user_profile.get(customer_id=borrower_id)
+            mobile_no = user_profile['mobile'] if user_profile else None
+
+            result.append({
+                'loan_id': emi['loan_id'],
+                'borrower_customer_id': borrower_id,
+                'borrower_full_name': emi['borrower_full_name'],
+                'borrower_email_id': user_profile['borrower_email_id'],
+                'mobile_no': mobile_no,
+                'default_fee': emi['default_fee']
+            })
         
         # Debug: Print the final result before setting it to the repeating panel
         print("Final result:", result)
@@ -38,9 +39,6 @@ class defaulters(defaultersTemplate):
         # Set the filtered data to the repeating panel
         self.repeating_panel_1.items = result
 
-
-    # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    open_form('admin.dashboard.accounting.mis_reports.behavioural_report')
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        open_form('admin.dashboard.accounting.mis_reports.behavioural_report')
