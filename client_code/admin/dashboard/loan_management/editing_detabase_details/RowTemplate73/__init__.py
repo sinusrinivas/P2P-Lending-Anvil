06f1a9a2002526dce71c20f4cf72f5758c1a885d
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-07-01 11:03:56 +0000

    Created admin.dashboard.loan_management.editing_detabase_details.RowTemplate73; Edited settings, admin.dashboard.loan_management.editing_detabase_details and admin.dashboard.loan_management.editing_detabase_details.RowTemplate72

--- client_code/admin/dashboard/loan_management/editing_detabase_details/RowTemplate73/__init__.py
+++ client_code/admin/dashboard/loan_management/editing_detabase_details/RowTemplate73/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import RowTemplate72Template
+from ._anvil_designer import RowTemplate73Template
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,18 +9,18 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class RowTemplate72(RowTemplate72Template):
+class RowTemplate73(RowTemplate73Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.refresh_data_bindings()
+
     # Any code you write here will run before the form opens.
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
-    # self.item['next_payment'] = self.date_picker_2.date
-    self.item['foreclosure_emi_num'] = int(self.text_box_3.text)
-    self.item['requested_on'] = self.date_picker_1.date
+    self.item['emi_number'] = int(self.text_box_3.text)
+    self.item['extension_request_date'] = self.date_picker_1.date
     self.item.update()
     self.refresh_data_bindings()
     alert('data saved')
--- client_code/admin/dashboard/loan_management/editing_detabase_details/__init__.py
+++ client_code/admin/dashboard/loan_management/editing_detabase_details/__init__.py
@@ -17,6 +17,7 @@ class editing_detabase_details(editing_detabase_detailsTemplate):
     self.repeating_panel_1.items = app_tables.fin_loan_details.search()
     self.repeating_panel_2.items = app_tables.fin_emi_table.search()
     self.repeating_panel_3.items = app_tables.fin_foreclosure.search()
+    self.repeating_panel_4.items = app_tables.fin_extends_loan.search()
   
   def button_1_copy_3_click(self, **event_args):
     """This method is called when the button is clicked"""
