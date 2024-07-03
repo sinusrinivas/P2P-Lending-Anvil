83f44f729e19573ef39e16a8748f0d22725135c1
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-07-03 04:26:13 +0000

    Edited admin.dashboard.loan_management.editing_detabase_details

--- client_code/admin/dashboard/loan_management/editing_detabase_details/__init__.py
+++ client_code/admin/dashboard/loan_management/editing_detabase_details/__init__.py
@@ -24,4 +24,51 @@ class editing_detabase_details(editing_detabase_detailsTemplate):
   def button_1_copy_3_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('admin.dashboard.loan_management')
+
+  def loan_details_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.data_grid_1.visible = True
+    self.data_grid_2.visible = False
+    self.data_grid_3.visible = False
+    self.data_grid_4.visible = False
+
+    self.loan_details_label.visible = True
+    self.emi_details_label.visible = False
+    self.extension_details_label.visible = False
+    self.foreclosure_details_label.visible = False
+
+  def emi_details_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.data_grid_1.visible = False
+    self.data_grid_2.visible = True
+    self.data_grid_3.visible = False
+    self.data_grid_4.visible = False
+
+    self.loan_details_label.visible = False
+    self.emi_details_label.visible = True
+    self.extension_details_label.visible = False
+    self.foreclosure_details_label.visible = False
     
+  def extension_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.data_grid_1.visible = False
+    self.data_grid_2.visible = False
+    self.data_grid_3.visible = True
+    self.data_grid_4.visible = False
+
+    self.loan_details_label.visible = False
+    self.emi_details_label.visible = False
+    self.extension_details_label.visible = True
+    self.foreclosure_details_label.visible = False
+
+  def foreclosure_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.data_grid_1.visible = False
+    self.data_grid_2.visible = False
+    self.data_grid_3.visible = False
+    self.data_grid_4.visible = True
+
+    self.loan_details_label.visible = False
+    self.emi_details_label.visible = False
+    self.extension_details_label.visible = False
+    self.foreclosure_details_label.visible = True
