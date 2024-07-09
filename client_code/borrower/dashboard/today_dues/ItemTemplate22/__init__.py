9b2ed6cb1bd3feccbfd7af278402e6d88d2929b9
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-07-09 07:18:04 +0000

    Edited borrower.dashboard.today_dues.ItemTemplate22

--- client_code/borrower/dashboard/today_dues/ItemTemplate22/__init__.py
+++ client_code/borrower/dashboard/today_dues/ItemTemplate22/__init__.py
@@ -12,14 +12,8 @@ from .. import main_form_module as main_form_module
 class ItemTemplate22(ItemTemplate22Template):
   def __init__(self, **properties):
     self.init_components(**properties)
-    # self.user_id = main_form_module.userId   
-    # user_data = app_tables.fin_loan_details.search()
-    # for row in user_data:
-    #     borrower_customer_id = row['borrower_customer_id']
-    #     lender_customer_id = row['lender_customer_id']
-    #     borrower_profile = app_tables.fin_user_profile.get(customer_id=borrower_customer_id)
-    #     lender_profile = app_tables.fin_user_profile.get(customer_id=lender_customer_id)
-    #     self.image_1.source = lender_profile['user_photo']
+    self.image_1.role = 'circular-image'
+
 
 
   def link_2_click(self, **event_args):
