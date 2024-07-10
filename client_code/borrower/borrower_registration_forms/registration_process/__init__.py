7384bb23bb0c0bea605d8b80759b071fb0804e1d
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-07-10 07:15:06 +0000

    Edited borrower.borrower_registration_forms.registration_process

--- client_code/borrower/borrower_registration_forms/registration_process/__init__.py
+++ client_code/borrower/borrower_registration_forms/registration_process/__init__.py
@@ -119,28 +119,34 @@ class registration_process(registration_processTemplate):
         
         user_data = app_tables.fin_user_profile.get(customer_id=user_id)
         
+        # List of labels to be highlighted based on form count
+        labels = [
+            (self.label_7, self.label_1),
+            (self.label_13, self.label_2),
+            (self.label_8, self.label_3),
+            (self.label_9, self.label_4),
+            (self.label_14, self.label_5),
+            (self.label_15, self.label_6)
+        ]
+        
+        # Set initial opacity for all labels
+        for label_pair in labels:
+            for label in label_pair:
+                label.foreground = "rgba(0, 0, 0, 0.5)"  # Set to semi-transparent
+
         if user_data is not None:
             form_count = user_data['form_count']
             print(form_count)
-            
-            # List of labels to be highlighted based on form count
-            labels = [
-                (self.label_7, self.label_1),
-                (self.label_13, self.label_2),
-                (self.label_8, self.label_3),
-                (self.label_9, self.label_4),
-                (self.label_14, self.label_5),
-                (self.label_15, self.label_6)
-            ]
             
             for i in range(form_count + 1):
                 if i < len(labels):
                     for label in labels[i]:
                         label.bold = True
+                        label.foreground = "rgba(0, 0, 0, 1)"  # Set to fully opaque
                         
                     # Set the icon for the first label in each pair
                     if i == 0:
-                        self.label_7.icon = '_/theme/bank_users/circle-1_1.png'
+                        self.label_7.icon = '_/theme/bank_users/circle-1_1.png'                    
                     elif i == 1:
                         self.label_8.icon = '_/theme/bank_users/circle-2%20(2).png'
                     elif i == 2:
