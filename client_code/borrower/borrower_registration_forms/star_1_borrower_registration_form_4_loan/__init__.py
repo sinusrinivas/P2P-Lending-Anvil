63ce209262032a94670de42ab8be7bddef59ce46
neeraja.bhupathi1696@gmail.com <neeraja.bhupathi1696@gmail.com>
2024-07-11 06:29:50 +0000

    Edited borrower.borrower_registration_forms.star_1_borrower_registration_form_4_loan

--- client_code/borrower/borrower_registration_forms/star_1_borrower_registration_form_4_loan/__init__.py
+++ client_code/borrower/borrower_registration_forms/star_1_borrower_registration_form_4_loan/__init__.py
@@ -22,6 +22,15 @@ class star_1_borrower_registration_form_4_loan(star_1_borrower_registration_form
         self.toggleswitch_2_interacted = False
         self.toggleswitch_3_interacted = False
         self.toggleswitch_4_interacted = False
+
+        self.label_1.visible = True
+        self.label_1.text = "No"
+        self.label_2.visible = True
+        self.label_2.text = "No"
+        self.label_7.visible = True
+        self.label_7.text = "No"
+        self.label_8.visible = True
+        self.label_8.text = "No"
         
         self.load_loan_status()
     
@@ -105,12 +114,12 @@ class star_1_borrower_registration_form_4_loan(star_1_borrower_registration_form
             self.update_loan_status('vehicle_loan', 'no')
     
     def next_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        # if not self.toggleswitch_1.checked or not self.toggleswitch_2.checked or not self.toggleswitch_3.checked or not self.toggleswitch_4.checked or not self.toggleswitch_1.unchecked or not self.toggleswitch_2.unchecked or not self.toggleswitch_3.unchecked or not self.toggleswitch_4.unchecked:
-        #     alert("Please select  loan status before proceeding.")
-        if not (self.toggleswitch_1_interacted and self.toggleswitch_2_interacted and self.toggleswitch_3_interacted and self.toggleswitch_4_interacted):
-            alert("Please select all loan status toggle switches Yes or No before proceeding.")
-        else:
+           """This method is called when the button is clicked"""
+        # # if not self.toggleswitch_1.checked or not self.toggleswitch_2.checked or not self.toggleswitch_3.checked or not self.toggleswitch_4.checked or not self.toggleswitch_1.unchecked or not self.toggleswitch_2.unchecked or not self.toggleswitch_3.unchecked or not self.toggleswitch_4.unchecked:
+        # #     alert("Please select  loan status before proceeding.")
+        # if not (self.toggleswitch_1_interacted and self.toggleswitch_2_interacted and self.toggleswitch_3_interacted and self.toggleswitch_4_interacted):
+        #     alert("Please select all loan status toggle switches Yes or No before proceeding.")
+        # else:
             anvil.server.call('add_borrower_step4', self.home_loan_status, 
                               self.other_loan_status, self.user_id,
                               self.credit_card_loan_status, self.vehicle_loan_status)
