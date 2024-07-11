b8b5a7a7f9f1efc100d2ad004b2cccfcc96de523
rahulganiger1709@gmail.com <rahulganiger1709@gmail.com>
2024-07-11 06:50:07 +0000

    Edited borrower.borrower_registration_forms.registration_process, lendor.lendor_registration_forms.registration_tracker_lender and lendor.lendor_registration_forms.lender_registration_form_1_education_form

--- client_code/lendor/lendor_registration_forms/registration_tracker_lender/__init__.py
+++ client_code/lendor/lendor_registration_forms/registration_tracker_lender/__init__.py
@@ -7,9 +7,9 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from .....bank_users.main_form import main_form_module
-from .....bank_users.main_form import main_form
-from .....bank_users.user_form import user_module
+from ....bank_users.main_form import main_form_module
+from ....bank_users.main_form import main_form
+from ....bank_users.user_form import user_module
 
 
 class registration_tracker_lender(registration_tracker_lenderTemplate):
@@ -26,7 +26,7 @@ class registration_tracker_lender(registration_tracker_lenderTemplate):
         
         # List of labels to be highlighted based on form count
         labels = [
-            (self.label_1),
+            (self.label_1,),
             (self.label_13, self.label_2),
             (self.label_14, self.label_3),
             (self.label_15, self.label_4),
@@ -57,7 +57,6 @@ class registration_tracker_lender(registration_tracker_lenderTemplate):
                         self.label_9.icon = '_/theme/bank_users/circle-3%20(2).png'
                     elif i == 3:
                         self.label_10.icon = '_/theme/bank_users/circle-4%20(2).png'
-                    elif i == 4:
+                    else:
                         self.label_11.icon = '_/theme/bank_users/circle-5%20(2).png'
-                    else :
-                        self.label_12.icon = '_/theme/bank_users/circle-6%20(2).png'
+                    
