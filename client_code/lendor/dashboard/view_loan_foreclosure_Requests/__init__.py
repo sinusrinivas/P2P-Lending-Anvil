5a335edd0b1f119ee481329cafdc989b9f1207b6
ganeshmirashi62@gmail.com <ganeshmirashi62@gmail.com>
2024-07-12 11:44:26 +0000

    Created lendor.dashboard.view_loan_foreclosure_Requests.ItemTemplate126 and lendor.dashboard.view_loan_foreclosure_Requests.ItemTemplate125; Edited borrower.dashboard.foreclosure_request.ItemTemplate118, borrower.dashboard.foreclosure_request.ItemTemplate95, lendor.dashboard.upcoming_payments.ItemTemplate1, lendor.dashboard.upcoming_payments.ItemTemplate94 and 1 more

--- client_code/lendor/dashboard/view_loan_foreclosure_Requests/__init__.py
+++ client_code/lendor/dashboard/view_loan_foreclosure_Requests/__init__.py
@@ -105,57 +105,117 @@ class view_loan_foreclosure_Requests(view_loan_foreclosure_RequestsTemplate):
     """This method is called when the button is clicked"""
     open_form("lendor.dashboard")  
 
+  # def button_3_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   self.label_2.visible = True
+  #   self.label_3.visible = False   
+  #   self.new.visible = False
+  #   self.label_1.visible = False
+  #   self.repeating_panel_6.visible = False
+  #   self.repeating_panel_5.visible = False  
+  #   self.repeating_panel_7.visible = True
+  #   self.repeating_panel_8.visible = False 
+  #   self.repeating_panel_9.visible = False
+
   def button_3_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.label_2.visible = True
-    self.label_3.visible = False   
-    self.new.visible = False
-    self.label_1.visible = False
-    self.repeating_panel_6.visible = False
-    self.repeating_panel_5.visible = False  
-    self.repeating_panel_7.visible = True
-    self.repeating_panel_8.visible = False 
-    self.repeating_panel_9.visible = False
+        """This method is called when the button is clicked (Approved)"""
+        self.label_2.visible = True
+        self.label_3.visible = False   
+        self.new.visible = False
+        self.label_1.visible = False
+        self.repeating_panel_3.visible = False
+        self.repeating_panel_4.visible = False
+        self.repeating_panel_5.visible = True
+        self.repeating_panel_6.visible = True
+        self.repeating_panel_7.visible = False
+        self.repeating_panel_8.visible = False
+        self.repeating_panel_1.visible = False
+        self.repeating_panel_2.visible = False
+    
+
+  # def button_2_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   self.label_3.visible = True 
+  #   self.new.visible = False
+  #   self.label_1.visible = False
+  #   self.label_2.visible = False
+  #   self.repeating_panel_6.visible = False
+  #   self.repeating_panel_5.visible = False  
+  #   self.repeating_panel_7.visible = False
+  #   self.repeating_panel_8.visible = True 
+  #   self.repeating_panel_9.visible = False
 
   def button_2_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.label_3.visible = True 
-    self.new.visible = False
-    self.label_1.visible = False
-    self.label_2.visible = False
-    self.repeating_panel_6.visible = False
-    self.repeating_panel_5.visible = False  
-    self.repeating_panel_7.visible = False
-    self.repeating_panel_8.visible = True 
-    self.repeating_panel_9.visible = False
+        """This method is called when the button is clicked (Rejected)"""
+        self.label_3.visible = True 
+        self.new.visible = False
+        self.label_1.visible = False
+        self.label_2.visible = False
+        self.repeating_panel_3.visible = False
+        self.repeating_panel_4.visible = False
+        self.repeating_panel_5.visible = False
+        self.repeating_panel_6.visible = False
+        self.repeating_panel_7.visible = True
+        self.repeating_panel_8.visible = True
+        self.repeating_panel_1.visible = False
+        self.repeating_panel_2.visible = False
 
+  # def button_5_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   #self.data_grid_4.visible = not self.data_grid_4.visible
+  #   self.new.visible = True    
+  #   self.label_2.visible = False
+  #   self.label_3.visible = False
+  #   self.label_1.visible = False
+  #   self.repeating_panel_6.visible = False
+  #   self.repeating_panel_5.visible = True
+  #   self.repeating_panel_7.visible = False
+  #   self.repeating_panel_8.visible = False 
+  #   self.repeating_panel_9.visible = False
 
   def button_5_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    #self.data_grid_4.visible = not self.data_grid_4.visible
-    self.new.visible = True    
-    self.label_2.visible = False
-    self.label_3.visible = False
-    self.label_1.visible = False
-    self.repeating_panel_6.visible = False
-    self.repeating_panel_5.visible = True
-    self.repeating_panel_7.visible = False
-    self.repeating_panel_8.visible = False 
-    self.repeating_panel_9.visible = False
+        """This method is called when the button is clicked (Under Process)"""
+        self.new.visible = True    
+        self.label_2.visible = False
+        self.label_3.visible = False
+        self.label_1.visible = False
+        self.repeating_panel_3.visible = False
+        self.repeating_panel_4.visible = False
+        self.repeating_panel_5.visible = False
+        self.repeating_panel_6.visible = False
+        self.repeating_panel_7.visible = False
+        self.repeating_panel_8.visible = False
+        self.repeating_panel_1.visible = True
+        self.repeating_panel_2.visible = True
     
 
+  # def button_6_click(self, **event_args):
+  #   """This method is called when the button is clicked"""
+  #   self.label_1.visible = True 
+  #   self.label_3.visible = False
+  #   self.new.visible = False
+  #   self.label_2.visible = False
+  #   self.repeating_panel_6.visible = True
+  #   self.repeating_panel_5.visible = False
+  #   self.repeating_panel_7.visible = False
+  #   self.repeating_panel_8.visible = False 
+  #   self.repeating_panel_9.visible = False
+
+
   def button_6_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.label_1.visible = True 
-    self.label_3.visible = False
-    self.new.visible = False
-    self.label_2.visible = False
-    self.repeating_panel_6.visible = True
-    self.repeating_panel_5.visible = False
-    self.repeating_panel_7.visible = False
-    self.repeating_panel_8.visible = False 
-    self.repeating_panel_9.visible = False
-    
+        """This method is called when the button is clicked (All)"""
+        self.label_1.visible = True 
+        self.label_3.visible = False
+        self.new.visible = False
+        self.label_2.visible = False
+        self.repeating_panel_3.visible = True
+        self.repeating_panel_4.visible = True
+        self.repeating_panel_5.visible = False
+        self.repeating_panel_6.visible = False
+        self.repeating_panel_7.visible = False
+        self.repeating_panel_8.visible = False
+        self.repeating_panel_1.visible = False
+        self.repeating_panel_2.visible = False
     
 
 
